3fabfb97bd10839a496fefb8822c2884d5c0da50
Matthew <matthewbegley@nhs.net>
2021-09-29 15:22:13 +0100

    Post commit auto formatting from 3rd swarm

--- application/producer.py
+++ application/producer.py
@@ -8,5 +8,15 @@ producer = KafkaProducer(bootstrap_servers="kafka:9092")
 while True:
     num = random.randint(0, 10)
     num_bytes = bytes(str(num), encoding="utf-8")
-    producer.send("TestTopic", json.dumps(['foo', {'bar': ('baz', None, 1.0, 2),}]).encode('utf-8'))
+    producer.send(
+        "TestTopic",
+        json.dumps(
+            [
+                "foo",
+                {
+                    "bar": ("baz", None, 1.0, 2),
+                },
+            ]
+        ).encode("utf-8"),
+    )
     time.sleep(1)
