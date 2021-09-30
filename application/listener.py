e7da036fdac8d6d1bb001c486cf653d81d98ab45
Jack Plowman <jack.plowman1@nhs.net>
2021-09-30 15:37:07 +0100

    Add source message from Azure Service Bus

---
+++ application/listener.py
@@ -0,0 +1,34 @@
+from azure.servicebus import ServiceBusClient
+from os import environ
+from requests import post
+
+
+def add_message_to_service_bus() -> None:
+    post(
+        url=environ["AZURE_SERVICE_BUS_ADD_MESSAGE_URL"],
+        headers={
+            "content-type": "application/json;charset=UTF-8",
+        },
+        json="{'key':'value'}",
+    )
+
+
+def receive_messages_from_service_bus() -> None:
+    with ServiceBusClient.from_connection_string(
+        conn_str=environ["AZURE_SERVICE_BUS_CONNECTION_STRING"],
+        logging_enable=True,
+    ) as service_bus_client:
+        print("ServiceBusClient set")
+        with service_bus_client.get_subscription_receiver(
+            topic_name=environ["AZURE_SERVICE_BUS_TOPIC_NAME"],
+            subscription_name=environ["AZURE_SERVICE_BUS_SUBSCRIPTION_NAME"],
+        ) as service_bus_receiver:
+            print("Topic and Subscription Name set")
+            received_msgs = service_bus_receiver.peek_messages(max_message_count=30)
+            print("Received messages if any")
+            print(received_msgs)
+
+
+if __name__ == "__main__":
+    # add_message_to_service_bus()
+    receive_messages_from_service_bus()
