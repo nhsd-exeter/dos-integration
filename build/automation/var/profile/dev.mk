3735f6f96e9ad3e7e0242fc25ef762df83f57031
Jack Plowman <jack.plowman1@nhs.net>
2021-10-29 08:21:37 +0100

    Add dev profile

--- build/automation/var/profile/dev.mk
+++ build/automation/var/profile/dev.mk
@@ -1,3 +1,6 @@
 -include $(VAR_DIR)/platform-texas/v1/account-live-k8s-nonprod.mk
 
 LOG_LEVEL := INFO
+
+TF_VAR_api_gateway_api_key_name := $(PROJECT_ID)-$(PROFILE)-api-key
+TF_VAR_nhs_uk_api_key_key := NHS_UK_API_KEY
