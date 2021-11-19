0dd667a7181217110907fe1cc8e2fcc1c43f644f
Ijong Maxwell <85811334+imax2emi@users.noreply.github.com>
2021-11-19 09:24:39 +0000

    refactored Docker code build

---
+++ infrastructure/stacks/docker-dev-1/variables.tf
@@ -0,0 +1,25 @@
+// Github variables
+variable "codestar_connection_arn" {
+  type        = string
+  description = "You should add Github connection"
+}
+
+variable "repository_id" {
+  type        = string
+ // eg. description = "Repository ID of the project ex: AbdellatifSfeir/sam-app"
+}
+
+variable "branch_name" {
+  type    = string
+  default = "develop"
+}
+
+// Lambda informations
+variable "prefix" {
+  type = string
+}
+
+variable "cloudformation_stack_name" {
+  type = string
+  description = "The name of the cloudformation project created by sam"
+}
