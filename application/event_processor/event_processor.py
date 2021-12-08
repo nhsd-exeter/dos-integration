18f801f032853adb9a049ef887ff1832636f0679
Ed Sparkes <1023491+ed-sparkes@users.noreply.github.com>
2021-12-08 12:07:01 +0000

    Some linting issue resolved

--- application/event_processor/event_processor.py
+++ application/event_processor/event_processor.py
@@ -7,6 +7,9 @@ from aws_lambda_powertools.utilities.typing.lambda_context import LambdaContext
 from boto3 import client
 from change_request import ChangeRequest
 from changes import get_changes
+
+from opening_times import spec_open_times_equal, spec_open_times_cr_format
+
 from common.utilities import invoke_lambda_function, is_mock_mode
 from dos import VALID_SERVICE_TYPES, VALID_STATUS_ID, DoSService, get_matching_dos_services
 from nhs import NHSEntity
