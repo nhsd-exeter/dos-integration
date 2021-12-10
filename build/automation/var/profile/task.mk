a6baf5f1f5d7aa91c777a4b01c25dc2d743d60ca
Jack Plowman <jack.plowman1@nhs.net>
2021-12-10 14:40:35 +0000

    Remove unnecessary variables and use common serverless variables

--- build/automation/var/profile/task.mk
+++ build/automation/var/profile/task.mk
@@ -3,7 +3,7 @@
 # ==============================================================================
 # Service variables
 
-LOG_LEVEL := DEBUG
+
 DOS_API_GATEWAY_SECRETS = $(TF_VAR_dos_api_gateway_secret)
 DOS_API_GATEWAY_USERNAME_KEY := DOS_API_GATEWAY_USERNAME
 DOS_API_GATEWAY_PASSWORD_KEY := DOS_API_GATEWAY_PASSWORD
