7c64f23d74e12fc3a7befc804bcb3294e6cea634
SLClayton <SLClayton@users.noreply.github.com>
2021-12-17 16:36:39 +0000

    Some changes to start

--- application/event_processor/nhs.py
+++ application/event_processor/nhs.py
@@ -1,6 +1,7 @@
 from datetime import date, datetime
 from itertools import groupby
-from typing import Dict, List
+from typing import Dict, List, Union
+from dataclasses import dataclass
 
 from aws_lambda_powertools import Logger
 
@@ -8,56 +9,49 @@ from opening_times import WEEKDAYS, OpenPeriod, SpecifiedOpeningTime, StandardOp
 
 logger = Logger(child=True)
 
-
+@dataclass
 class NHSEntity:
     """This is an object to store an NHS Entity data
 
-    When passed in a payload (dict) with the NHS data, it will
-    pass those fields in to the object as attributes.
-
-    This object may be added to with methods to make the
-    comparions with services easier in future tickets.
-
+    Some fields are pulled straight from the payload while others are processed first. So attribute
+    names differ from paylod format for consistency within object.    
     """
+    entity_data: dict
+    odscode: str
+    org_name: str
+    org_type_id: str
+    org_type: str
+    org_sub_type: str
+    org_status: str
+    address_lines: List[str]
+    postcode: str
+    standard_opening_times: Union[StandardOpeningTimes, None]
+    specified_opening_times: Union[List[SpecifiedOpeningTime], None]
 
-    CLOSED_AND_HIDDEN_STATUSES = ["Hidden", "Closed"]
+    CLOSED_AND_HIDDEN_STATUSES = ["HIDDEN", "CLOSED"]
 
-    def __init__(self, entity_data: dict) -> None:
-        # Set attributes for each value in dict
-        self.data = entity_data
-        for key, value in entity_data.items():
-            setattr(self, key, value)
-
-        self._standard_opening_times = None
-        self._specified_opening_times = None
+    def __init__(self, entity_data: dict):
+        self.entity_data = entity_data
+        self.odscode = entity_data["ODSCode"]
+        self.org_name = entity_data["OrganisationName"]
+        self.org_type_id = entity_data["OrganisationTypeId"]
+        self.org_sub_type = entity_data["OrganisationSubType"]
+        self.org_status = entity_data["OrganisationStatus"]
+        self.odscode = entity_data["ODSCode"]
+        self.postcode = entity_data["Postcode"]
+        self.address_lines = [
+            line for line in [entity_data.get(k) for k in [f"Address{i}" for i in range(1, 5)] + ["City", "County"]]
+            if isinstance(line, str) and line.strip() != ""]
+        self.standard_opening_times = self._get_standard_opening_times()
+        self.specified_opening_times = self._get_specified_opening_times()
 
     def __repr__(self) -> str:
-        return f"<NHSEntity: name={self.OrganisationName} odscode={self.ODSCode}>:"
+        return f"<NHSEntity: name={self.org_name} odscode={self.odscode}>"
 
     def normal_postcode(self):
-        return self.Postcode.replace(" ", "").upper()
+        return self.postcode.replace(" ", "").upper()
 
-    def get_standard_opening_times(self) -> StandardOpeningTimes:
-        """[summary]
-
-        Returns:
-            StandardOpeningTimes: [description]
-        """
-        if self._standard_opening_times is None:
-            self._standard_opening_times = self._get_standard_opening_times("General")
-        return self._standard_opening_times
-
-    def get_specified_opening_times(self) -> List[SpecifiedOpeningTime]:
-        """[summary]
-
-        Returns:
-            List[SpecifiedOpeningTime]: [description]
-        """
-        if self._specified_opening_times is None:
-            self._specified_opening_times = self._get_specified_opening_times("General")
-        return self._specified_opening_times
-
-    def _get_standard_opening_times(self, opening_time_type: str) -> StandardOpeningTimes:
+    def _get_standard_opening_times(self) -> StandardOpeningTimes:
         """Filters the raw opening times data for standard weekly opening
         times and returns it in a StandardOpeningTimes object.
 
@@ -69,23 +63,25 @@ class NHSEntity:
         """
         std_opening_times = StandardOpeningTimes()
         for open_time in self.OpeningTimes:
-            # Skips unwanted open times
-            if not (
-                open_time["Weekday"].lower() in WEEKDAYS
-                and open_time["AdditionalOpeningDate"] == ""
-                and open_time["OpeningTimeType"] == opening_time_type
-                and open_time["IsOpen"]
-            ):
-                continue
-
             weekday = open_time["Weekday"].lower()
-            start, end = [datetime.strptime(time_str, "%H:%M").time() for time_str in open_time["Times"].split("-")]
-            open_period = OpenPeriod(start, end)
-            std_opening_times.add_open_period(open_period, weekday)
 
+            # Skip times which are not Standard (General) Opening times
+            if not (weekday in WEEKDAYS and
+                    open_time["AdditionalOpeningDate"] in ["", None] and 
+                    open_time["OpeningTimeType"] == "General"):
+                continue
+            
+            # Populate StandardOpeningTimes obj depending on IsOpen status
+            if open_time["IsOpen"]:
+                start, end = [datetime.strptime(time_str, "%H:%M").time() for time_str in open_time["Times"].split("-")]
+                open_period = OpenPeriod(start, end)
+                std_opening_times.add_open_period(open_period, weekday)
+            else:
+                std_opening_times.closed_days.add(weekday)
+        
         return std_opening_times
 
-    def _get_specified_opening_times(self, opening_time_type: str) -> List[SpecifiedOpeningTime]:
+    def _get_specified_opening_times(self) -> List[SpecifiedOpeningTime]:
         """Get all the Specified Opening Times
 
         Args:
@@ -97,27 +93,32 @@ class NHSEntity:
 
         # Filter
         def specified_opening_times_filter(specified):
-            return specified["OpeningTimeType"] == opening_time_type and specified["AdditionalOpeningDate"] != ""
+            return specified["OpeningTimeType"] == "Additional" and specified["AdditionalOpeningDate"] != ""
 
-        specified_times_list = list(filter(specified_opening_times_filter, self.OpeningTimes))
+        specified_times_list = list(filter(specified_opening_times_filter, self.entity_data["OpeningTimes"]))
 
         # Sort the openingtimes data
         sort_specified = sorted(specified_times_list, key=lambda item: (item["AdditionalOpeningDate"], item["Times"]))
-        specified_opening_time_dict: Dict[datetime, List[OpenPeriod]] = {}
+        specified_opening_time_dict: Dict[str, List[OpenPeriod]] = {}
+        specified_closed_days = set()
 
         # Grouping data by date
-        key: date
-
-        for key, value in groupby(sort_specified, lambda item: (item["AdditionalOpeningDate"])):
+        for date_str, values in groupby(sort_specified, lambda item: (item["AdditionalOpeningDate"])):
             op_list: List[OpenPeriod] = []
-            for item in list(value):
-                start, end = [datetime.strptime(time_str, "%H:%M").time() for time_str in item["Times"].split("-")]
-                op_list.append(OpenPeriod(start, end))
-                specified_opening_time_dict[key] = op_list
+            for item in list(values):
+                if item["IsOpen"]:
+                    start, end = [datetime.strptime(time_str, "%H:%M").time() for time_str in item["Times"].split("-")]
+                    op_list.append(OpenPeriod(start, end))
+                    specified_opening_time_dict[date_str] = op_list
+                else:
+                    specified_closed_days.append(date_str)
 
         specified_opening_times = [
-            SpecifiedOpeningTime(value, datetime.strptime(key, "%b  %d  %Y").date())
-            for key, value in specified_opening_time_dict.items()
+            SpecifiedOpeningTime(
+                open_periods=open_periods, 
+                date=datetime.strptime(date_str, "%b  %d  %Y").date(),
+                is_open=(date_str not in specified_closed_days))
+            for date_str, open_periods in specified_opening_time_dict.items()
         ]
 
         return specified_opening_times
@@ -128,4 +129,4 @@ class NHSEntity:
         Returns:
             bool: True if status is hidden or closed, False otherwise
         """
-        return self.OrganisationStatus in self.CLOSED_AND_HIDDEN_STATUSES
+        return self.org_status.upper() in self.CLOSED_AND_HIDDEN_STATUSES
--- application/event_processor/opening_times.py
+++ application/event_processor/opening_times.py
@@ -1,6 +1,8 @@
 from dataclasses import dataclass
 from datetime import date, time
-from typing import Any, Dict, List
+from inspect import getargs
+from os import stat
+from typing import Any, Dict, List, Union
 
 import change_request
 from aws_lambda_powertools import Logger
@@ -28,10 +30,6 @@ class OpenPeriod:
     def __eq__(self, other: Any):
         return isinstance(other, OpenPeriod) and self.start == other.start and self.end == other.end
 
-    """ For all comparison methods, check start value first or check
-        end value if starts are equal
-    """
-
     def __lt__(self, other: Any):
         if self.start == other.start:
             return self.end < other.end
@@ -48,38 +46,74 @@ class OpenPeriod:
     def overlaps(self, other) -> bool:
         assert self.start_before_end()
         assert other.start_before_end()
-
         return self.start < other.end and other.start < self.end
 
-    def export_cr_format(self) -> Dict[str, str]:
+    def export_cr_format(self) -> Union[Dict[str, str], None]:
         """Exports open period into a DoS change request accepted format"""
+        if not self.is_open:
+            return None
         return {
             "start_time": self.start.strftime(change_request.TIME_FORMAT),
             "end_time": self.end.strftime(change_request.TIME_FORMAT),
         }
 
+    @staticmethod
+    def any_overlaps(open_periods: List['OpenPeriod']) -> bool:
+        """Returns whether any OpenPeriod object in list overlaps any others in the list"""
+        untested = open_periods.copy()
+        while len(untested) > 1:
+            test_op = untested.pop(0)
+            for op in untested:
+                if test_op.overlaps(op):
+                    return True
+        return False
+
+    @staticmethod
+    def list_string(open_periods: List['OpenPeriod']) -> str:
+        """Returns a string version of a list of open periods in a consistently sorted order
+
+        eg.
+        '[08:00:00-13:00:00, 14:00:00-17:00:00, 18:00:00-20:00:00]
+        """
+        sorted_str_list = [str(op) for op in sorted(list(open_periods))]
+        return f"[{', '.join(sorted_str_list)}]"
+
+    @staticmethod
+    def any_start_before_end(open_periods: List['OpenPeriod']) -> bool:
+        """Returns whether any OpenPeriod object in list starts before it ends"""
+        for op in open_periods:
+            if op.start_before_end():
+                return True
+        return False
+
+    @staticmethod
+    def equal_lists(a: List['OpenPeriod'], b: List['OpenPeriod']) -> bool:
+        """Checks equality between 2 lists of open periodsRelies on sorting and eq functions in OpenPeriod"""
+        return sorted(a) == sorted(b)
+
 
 @dataclass(unsafe_hash=True)
 class SpecifiedOpeningTime:
-    def __init__(self, open_periods: (List[OpenPeriod]), specified_date: (date)):
+    def __init__(self, open_periods: List[OpenPeriod], specified_date: date, is_open: bool=True):
         assert isinstance(specified_date, date)
         self.open_periods = open_periods
         self.date = specified_date
+        self.is_open = is_open
 
     def date_string(self) -> str:
         return self.date.strftime("%d-%m-%Y")
 
-    def openings_string(self) -> str:
-        return open_periods_string(self.open_periods)
+    def open_periods_string(self) -> str:
+        return OpenPeriod.list_string(self.open_periods)
 
     def __repr__(self):
-        return f"<SpecifiedOpenTime: {self.date_string()} {self.openings_string()}>"
+        return f"<SpecifiedOpenTime: {self.date_string()} {self.open_periods_string()}>"
 
     def __eq__(self, other):
         return (
             isinstance(other, SpecifiedOpeningTime)
             and self.date == other.date
-            and open_periods_equal(self.open_periods, other.open_periods)
+            and OpenPeriod.equal_lists(self.open_periods, other.open_periods)
         )
 
     def export_cr_format(self) -> dict:
@@ -88,6 +122,30 @@ class SpecifiedOpeningTime:
         date_str = self.date.strftime(change_request.DATE_FORMAT)
         change = {date_str: exp_open_periods}
         return change
+
+    def open_contradiction(self) -> bool:
+        """Returns whether the open flag contradicts the number of open periods present."""
+        return self.is_open != len(self.open_periods) > 0
+
+    def any_overlaps(self):
+        return OpenPeriod.any_overlaps(self.open_periods)
+
+    @staticmethod
+    def export_cr_format_list(spec_opening_dates: List['SpecifiedOpeningTime']) -> dict:
+        """Runs the export_cr_format on a list of SpecifiedOpeningTime objects and combines the results"""
+        opening_dates_cr_format = {}
+        for spec_open_date in spec_opening_dates:
+            spec_open_date_payload = spec_open_date.export_cr_format()
+            opening_dates_cr_format.update(spec_open_date_payload)
+        return opening_dates_cr_format
+
+    @staticmethod
+    def equal_lists(a: List['SpecifiedOpeningTime'], b: List['SpecifiedOpeningTime']) -> bool:
+        """Checks equality between 2 lists of SpecifiedOpeningTime Relies on equality,
+        and hash functions of SpecifiedOpeningTime"""
+        hash_list_a = [hash(a) for a in a]
+        hash_list_b = [hash(b) for b in b]
+        return sorted(hash_list_a) == sorted(hash_list_b)
 
 
 @dataclass(unsafe_hash=True)
@@ -106,19 +164,23 @@ class StandardOpeningTimes:
         # Initialise all weekday OpenPeriod lists as empty
         for day in WEEKDAYS:
             setattr(self, day, [])
+        self.closed_days = set()
 
-    def __str__(self):
-        day_opening_strs = [f"{day}:{open_periods_string(getattr(self, day))}" for day in WEEKDAYS]
-        return ", ".join(day_opening_strs)
+    def __repr__(self):
+        day_opening_strs = [f"{day}={OpenPeriod.list_string(getattr(self, day))}" for day in WEEKDAYS]
+        return f"<StandardOpeningTimes: {' '.join(day_opening_strs)}>"
 
     def __len__(self):
         return sum([len(getattr(self, day)) for day in WEEKDAYS])
 
     def __eq__(self, other: Any):
         for day in WEEKDAYS:
-            if not open_periods_equal(getattr(self, day), getattr(other, day)):
+            if not OpenPeriod.equal_lists(getattr(self, day), getattr(other, day)):
                 return False
         return True
+
+    def is_open(self, weekday: str) -> bool:
+        return len(getattr(self, weekday)) > 0
 
     def add_open_period(self, open_period: OpenPeriod, weekday: str) -> None:
         """Adds a formatted open period to the specified weekda
@@ -127,10 +189,24 @@ class StandardOpeningTimes:
             open_period (OpenPeriod): The open period to add
             weekday (str): The weekday to add the open period to
         """
-        if weekday in WEEKDAYS:
-            getattr(self, weekday).append(open_period)
+        day_key = weekday.lower()
+        if day_key in WEEKDAYS:
+            getattr(self, day_key).append(open_period)
         else:
             logger.error(f"Cannot add opening time for invalid weekday '{weekday}', open period not added.")
+
+    def any_overlaps(self):
+        for weekday in WEEKDAYS:
+            if OpenPeriod.any_overlaps(getattr(self, weekday)):
+                return True
+        return False
+
+    def any_contradictions(self) -> bool:
+        """Returns True if any open period falls on a day that is marked as closed."""
+        for weekday in self.closed_days:
+            if len(getattr(self, weekday)) > 0:
+                return True
+        return False
 
     def export_cr_format(self) -> Dict[str, List[Dict[str, str]]]:
         """Exports standard opening times into a DoS change request accepted format"""
@@ -141,52 +217,3 @@ class StandardOpeningTimes:
         return change
 
 
-def open_periods_string(open_periods: List[OpenPeriod]) -> str:
-    """Returns a string version of a list of open periods in a consistently sorted order
-
-    eg.
-    '[08:00:00-13:00:00, 14:00:00-17:00:00, 18:00:00-20:00:00]
-    """
-    sorted_str_list = [str(op) for op in sorted(list(open_periods))]
-    return f"[{', '.join(sorted_str_list)}]"
-
-
-def any_overlaps(open_periods: List[OpenPeriod]) -> bool:
-    """Returns whether any OpenPeriod object in list overlaps any others in the list"""
-    untested = open_periods.copy()
-    while len(untested) > 1:
-        test_op = untested.pop(0)
-        for op in untested:
-            if test_op.overlaps(op):
-                return True
-    return False
-
-
-def any_start_before_end(open_periods: List[OpenPeriod]) -> bool:
-    """Returns whether any OpenPeriod object in list starts before it ends"""
-    for op in open_periods:
-        if op.start_before_end():
-            return True
-    return False
-
-
-def spec_open_times_cr_format(spec_opening_dates: List[SpecifiedOpeningTime]) -> dict:
-    """Runs the export_cr_format on a list of SpecifiedOpeningTime objects and combines the results"""
-    opening_dates_cr_format = {}
-    for spec_open_date in spec_opening_dates:
-        spec_open_date_payload = spec_open_date.export_cr_format()
-        opening_dates_cr_format.update(spec_open_date_payload)
-    return opening_dates_cr_format
-
-
-def open_periods_equal(a: List[OpenPeriod], b: List[OpenPeriod]) -> bool:
-    """Checks equality between 2 lists of open periodsRelies on sorting and eq functions in OpenPeriod"""
-    return sorted(a) == sorted(b)
-
-
-def spec_open_times_equal(a: List[SpecifiedOpeningTime], b: List[SpecifiedOpeningTime]) -> bool:
-    """Checks equality between 2 lists of SpecifiedOpeningTime Relies on equality,
-    and hash functions of SpecifiedOpeningTime"""
-    hash_list_a = [hash(a) for a in a]
-    hash_list_b = [hash(b) for b in b]
-    return sorted(hash_list_a) == sorted(hash_list_b)
