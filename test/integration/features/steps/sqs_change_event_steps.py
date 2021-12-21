58f3380202b907d8f7a0f93cee431801d6fee7f2
mik3ola <michael.olaw1@nhs.net>
2021-12-21 15:56:05 +0000

    Add secrets script

--- test/integration/features/steps/sqs_change_event_steps.py
+++ test/integration/features/steps/sqs_change_event_steps.py
@@ -1,6 +1,8 @@
 from os import getenv
-from behave import given, then, when
+from behave import given, then, when, And
 from features.utilities.utilities import get_response
+from features.utilities.get_secrets import get_secret
+import json
 
 
 @given("a valid change request endpoint")
@@ -11,4 +13,6 @@ def a_valid_change_request_endpoint(context):
 @when('a "{change_request_status}" change request is sent to the event sender')
 def a_valid_change_request_is_sent_to_the_event_sender(context, change_request_status: str):
     response = get_response()
+    secret = json.loads(get_secret())
     print(response.text)
+    print(secret)
---
+++ test/integration/features/utilities/get_secrets.py
@@ -0,0 +1,42 @@
+import boto3
+from botocore.exceptions import ClientError
+from os import getenv
+
+
+def get_secret():
+
+    secret_name = getenv("API_KEY_SECRET")
+    region_name = "eu-west-2"
+
+
+    # Create a Secrets Manager client
+    session = boto3.session.Session()
+    client = session.client(
+        service_name='secretsmanager',
+        region_name=region_name
+    )
+
+    try:
+        get_secret_value_response = client.get_secret_value(
+            SecretId=secret_name
+        )
+    except ClientError as e:
+        if e.response['Error']['Code'] == 'InvalidParameterException':
+            # You provided an invalid value for a parameter.
+            # Deal with the exception here, and/or rethrow at your discretion.
+            raise e
+        elif e.response['Error']['Code'] == 'InvalidRequestException':
+            # You provided a parameter value that is not valid for the current state of the resource.
+            # Deal with the exception here, and/or rethrow at your discretion.
+            raise e
+        elif e.response['Error']['Code'] == 'ResourceNotFoundException':
+            # We can't find the resource that you asked for.
+            # Deal with the exception here, and/or rethrow at your discretion.
+            raise e
+    else:
+        # Decrypts secret using the associated KMS CMK.
+        # Depending on whether the secret is a string or binary, one of these fields will be populated.
+        # if 'SecretString' in get_secret_value_response:
+        secret = get_secret_value_response['SecretString']
+
+    return secret
