d26110398c56675c0ec91d0c00cf7785befee9cf
Kit <christopher.corbishley1@nhs.net>
2022-01-06 15:21:19 +0000

    Added files with payloads

--- test/integration/features/steps/happy_path_steps.py
+++ test/integration/features/steps/happy_path_steps.py
@@ -1,6 +1,6 @@
 from os import getenv
 from behave import given, then, when
-from features.utilities.utilities import process_change_event
+from features.utilities.utilities import process_change_event, get_response
 # from features.utilities.get_secrets import get_secret
 # import json
 
@@ -14,3 +14,10 @@ def a_valid_change_request_endpoint(context):
 def a_valid_change_request_is_sent_to_the_event_sender(context):
     response = process_change_event()
     assert response['Message'] == "Change event received", "ERROR! Invalid Payload received.."
+
+
+@when('a "{change_request_status}" change request is sent to the event sender')
+def a_valid_change_request_is_sent_to_the_event_sender(context, change_request_status: str):
+    response = get_response(change_request_status)
+    assert response['Message'] == "Change event received", "ERROR! Invalid Payload received.."
+
