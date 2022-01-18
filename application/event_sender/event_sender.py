178d4e1e5ad87322e19963f0590c94a6639a89c7
Ed Sparkes <1023491+ed-sparkes@users.noreply.github.com>
2022-01-18 08:10:28 +0000

    Log message latency in sender

--- application/event_sender/event_sender.py
+++ application/event_sender/event_sender.py
@@ -2,6 +2,8 @@ from typing import Any, Dict
 
 from aws_lambda_powertools import Tracer
 from aws_lambda_powertools import Logger
+from time import time_ns
+from aws_embedded_metrics import metric_scope
 from aws_lambda_powertools.utilities.typing import LambdaContext
 from change_request import ChangeRequest
 from common.middlewares import unhandled_exception_logging
@@ -13,7 +15,8 @@ logger = Logger()
 
 @tracer.capture_lambda_handler()
 @unhandled_exception_logging
-def lambda_handler(event: Dict[str, Any], context: LambdaContext) -> None:
+@metric_scope
+def lambda_handler(event: Dict[str, Any], context: LambdaContext, metrics) -> None:
     """Entrypoint handler for the event_sender lambda
 
     Args:
@@ -26,6 +29,11 @@ def lambda_handler(event: Dict[str, Any], context: LambdaContext) -> None:
         "Received change request",
         extra={"change_request": body["change_payload"], "correlation_id": logger.get_correlation_id()},
     )
+    message_received = body["message_received"]
+
     change_request = ChangeRequest(body["change_payload"])
     change_request.post_change_request()
+    now_ms = time_ns() // 1000000
+    # TODO: Only record latency for success?
+    metrics.put_metric("ProcessingLatency", now_ms - message_received, "Milliseconds")
     return change_request.get_response()
