7d236f7142178235d40ed50a84e9da5bc46f387b
Jack Plowman <jack.plowman1@nhs.net>
2022-01-20 09:48:01 +0000

    Update to new variable names and add odscode files

--- test/performance/create_change_events/utilities.py
+++ test/performance/create_change_events/utilities.py
@@ -14,18 +14,24 @@ def setup_change_event_request(json_file_path: str) -> tuple[dict[str, str], str
     Returns:
         tuple[dict[str, str], str]: Tuple containing the headers and json payload
     """
-    default_headers = {
-        "x-api-key": None,
-        "sequence-number": str(round(time() * 1000)),
-    }
+    headers = setup_headers()
+    payload = load(open(json_file_path, "r+"))
+    return headers, payload
+
+
+def setup_headers() -> dict:
+    headers = {"sequence-number": str(round(time() * 1000))}
     api_key_json = get_secret(getenv("API_KEY_SECRET_NAME"))
     api_key = loads(api_key_json)[getenv("API_KEY_SECRET_KEY")]
-    default_headers["x-api-key"] = api_key
-    json_payload = load(open(json_file_path, "r+"))
-    return default_headers, json_payload
+    headers["x-api-key"] = api_key
+    return headers
 
 
-def make_change_event_unique(json_payload: dict) -> dict:
+def get_ods_code() -> str:
+    pass
+
+
+def make_change_event_unique(payload: dict) -> dict:
     """Make the change event unique by adding a unique id to the payload
 
     Args:
@@ -34,8 +40,8 @@ def make_change_event_unique(json_payload: dict) -> dict:
     Returns:
         dict: The json payload with the unique id added
     """
-    json_payload["id"] = str(time())
-    return json_payload
+    payload["id"] = str(time())
+    return payload
 
 
 def get_secret(secret_name: str) -> str:
