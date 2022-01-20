ca56938002c5755c9a868122c256a7fcf243e201
Ed Sparkes <1023491+ed-sparkes@users.noreply.github.com>
2022-01-20 09:08:08 +0000

    Fix the formatting

--- test/integration/features/utilities/change_events.py
+++ test/integration/features/utilities/change_events.py
@@ -1,16 +1,20 @@
 from json import load, dumps
 
+
 def load_json_file() -> dict:
     with open(f"./features/resources/payloads/11_expected_schema.json", "r", encoding="utf-8") as json_file:
         return load(json_file)
+
 
 def get_change_event_string() -> str:
     change_event = load_json_file()
     change_event = dumps(change_event)
     return change_event
 
+
 def get_change_event_dict() -> dict:
     return load_json_file()
+
 
 def compile_change_event(change_event: dict) -> dict:
     # Returns:
--- test/integration/features/utilities/utils.py
+++ test/integration/features/utilities/utils.py
@@ -16,18 +16,19 @@ dynamo_db_table = getenv("DYNAMO_DB_TABLE")
 lambda_client_functions = client("lambda")
 sqs_client = client("sqs")
 dynamo_client = client("dynamodb")
-dynamodb = boto3.resource('dynamodb')
+dynamodb = boto3.resource("dynamodb")
 
 
 def process_payload(payload: dict) -> str:
     headers = {
-    'x-api-key': json.loads(get_secret())[getenv('NHS_UK_API_KEY')],
-    'sequence-number': str(get_new_sequence_id_for_a_given_odscode(payload["ODSCode"])),
-    'Content-Type': 'application/json'
+        "x-api-key": json.loads(get_secret())[getenv("NHS_UK_API_KEY")],
+        "sequence-number": str(get_new_sequence_id_for_a_given_odscode(payload["ODSCode"])),
+        "Content-Type": "application/json",
     }
     output = requests.request("POST", url, headers=headers, data=dumps(payload))
     sleep(30)
     return output
+
 
 def debug_purge_queue():
     try:
@@ -35,26 +36,29 @@ def debug_purge_queue():
     except Exception as e:
         print(f"ERROR!..UNABLE TO PURGE. {e}")
 
+
 def get_stored_events_from_db(seq_id: str) -> dict:
     table = dynamodb.Table(dynamo_db_table)
     sequence_number = seq_id
-    response = table.scan(
-        FilterExpression=Attr("SequenceNumber").eq(Decimal(sequence_number)))
+    response = table.scan(FilterExpression=Attr("SequenceNumber").eq(Decimal(sequence_number)))
     items = response["Items"]
     for item in items:
         return item
     # return json.loads(items, indent=2, default=str)
 
+
 def get_response(payload: str, corr_id: str) -> str:
     payload_name = payload
     correlation_id = corr_id
     response = process_change_event(payload_name, correlation_id)
-    return response.json()['message']
+    return response.json()["message"]
+
 
 # This matches a payload file with a string describing it from the Steps
 
+
 def get_payload(payload_name: str) -> str:
-    values = {"valid" : "11_expected_schema.json", "invalid" : "10_invalid.json"}
+    values = {"valid": "11_expected_schema.json", "invalid": "10_invalid.json"}
     if payload_name in ["valid", "invalid"]:
         payload_file_name = values[payload_name]
     else:
@@ -62,13 +66,13 @@ def get_payload(payload_name: str) -> str:
     with open(f"./features/resources/payloads/{payload_file_name}", "r", encoding="utf-8") as json_file:
         return dumps(load(json_file))
 
+
 def get_lambda_info(info_param: str) -> str:
-    values = {"state" : "State", "status" : "LastUpdateStatus", "description": "Description"}
+    values = {"state": "State", "status": "LastUpdateStatus", "description": "Description"}
     param = values[info_param]
-    response = lambda_client_functions.get_function(
-            FunctionName=event_processor
-    )
+    response = lambda_client_functions.get_function(FunctionName=event_processor)
     return response["Configuration"][param]
+
 
 def get_new_sequence_id_for_a_given_odscode(odscode: str) -> int:
     # Get latest sequence id for a given odscode from dynamodb
@@ -90,4 +94,4 @@ def get_new_sequence_id_for_a_given_odscode(odscode: str) -> int:
     except Exception as err:
         print(f"Unable to get sequence id from dynamodb for a given ODSCode {odscode} .Error: {err}")
         raise
-    return sequence_number+1
+    return sequence_number + 1
