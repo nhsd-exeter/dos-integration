09b27093daff8f679e88eab7b6f643cb2527680e
Ed Sparkes <1023491+ed-sparkes@users.noreply.github.com>
2022-01-20 07:53:23 +0000

    Formatted the file

--- application/event_sender/tests/test_event_sender.py
+++ application/event_sender/tests/test_event_sender.py
@@ -49,19 +49,23 @@ def lambda_context():
 
     return LambdaContext()
 
+
 class MockResponse:
     def __init__(self, status_code, text=""):
         self.status_code = status_code
         self.text = text
 
+
 @patch(f"{FILE_PATH}.ChangeRequest")
 @patch(f"{FILE_PATH}.time_ns", return_value=1642619746522500523)
-@patch.object(MetricsLogger,'put_metric')
-@patch.object(MetricsLogger,'put_dimensions')
-def test_lambda_handler_dos_api_success(mock_put_dimension, mock_put_metric,mock_time, mock_change_request, lambda_context, mock_logger):
+@patch.object(MetricsLogger, "put_metric")
+@patch.object(MetricsLogger, "put_dimensions")
+def test_lambda_handler_dos_api_success(
+    mock_put_dimension, mock_put_metric, mock_time, mock_change_request, lambda_context, mock_logger
+):
 
     mock_instance = mock_change_request.return_value
-    mock_instance.post_change_request.return_value = MockResponse(status_code=200,text="success")
+    mock_instance.post_change_request.return_value = MockResponse(status_code=200, text="success")
     environ["ENV"] = "test"
     environ["DOS_API_GATEWAY_REQUEST_TIMEOUT"] = "1"
 
@@ -71,19 +75,21 @@ def test_lambda_handler_dos_api_success(mock_put_dimension, mock_put_metric,mock
 
     mock_change_request.assert_called_once_with(CHANGE_REQUEST)
     mock_instance.post_change_request.assert_called_once_with()
-    mock_put_dimension.assert_called_once_with({"ENV":"test"})
+    mock_put_dimension.assert_called_once_with({"ENV": "test"})
     mock_put_metric.assert_called_once_with("ProcessingLatency", 3000, "Milliseconds")
     assert response["statusCode"] == 200
     assert response["body"] == "success"
 
-@patch(f"{FILE_PATH}.ChangeRequest")
-@patch.object(MetricsLogger,'put_metric')
-@patch.object(MetricsLogger,'put_dimensions')
-def test_lambda_handler_dos_api_fail(mock_put_dimension, mock_put_metric, mock_change_request, lambda_context, mock_logger):
 
+@patch(f"{FILE_PATH}.ChangeRequest")
+@patch.object(MetricsLogger, "put_metric")
+@patch.object(MetricsLogger, "put_dimensions")
+def test_lambda_handler_dos_api_fail(
+    mock_put_dimension, mock_put_metric, mock_change_request, lambda_context, mock_logger
+):
 
     mock_instance = mock_change_request.return_value
-    mock_instance.post_change_request.return_value = MockResponse(status_code=500,text="something went wrong")
+    mock_instance.post_change_request.return_value = MockResponse(status_code=500, text="something went wrong")
 
     environ["ENV"] = "test"
     # Act
@@ -92,7 +98,7 @@ def test_lambda_handler_dos_api_fail(mock_put_dimension, mock_put_metric, mock_c
 
     mock_change_request.assert_called_once_with(CHANGE_REQUEST)
     mock_change_request().post_change_request.assert_called_once_with()
-    mock_put_dimension.assert_called_once_with({"ENV":"test"})
+    mock_put_dimension.assert_called_once_with({"ENV": "test"})
     mock_put_metric.assert_called_once_with("DoSApiFail", 1, "Count")
     assert response["statusCode"] == 500
     assert response["body"] == "something went wrong"
