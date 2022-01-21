6f0d0920122050abd7a258760da36a20df592355
mik3ola <michael.olaw1@nhs.net>
2022-01-21 01:23:33 +0000

    Fix dynamo query for event pulls

--- test/integration/features/steps/parent_steps.py
+++ test/integration/features/steps/parent_steps.py
@@ -6,45 +6,62 @@ from features.utilities.utils import (
     debug_purge_queue,
     process_payload,
     get_stored_events_from_db,
-    get_payload,
+    get_latest_sequence_id_for_a_given_odscode,
 )
+from decimal import Decimal
 from features.utilities.change_events import get_change_event_string, get_change_event_dict
 from features.utilities.log_stream import get_logs
 import json
+from time import sleep
 
 
-@given("a valid change event is provided")
-def a_change_event_is_provided(context):
+@given("a Changed Event is valid")
+def a_change_event_is_valid(context):
     """Creates a valid change event"""
     context.change_event = get_change_event_string()
 
 
-@given("a change event with invalid ODSCode is provided")
-def a_change_event_is_provided(context):
+@given("a Changed Event has no matching DoS services")
+def a_change_event_with_no_matching_dos_services(context):
+    context.change_event = get_change_event_string()
+    context.change_event["ODSCode"] = "CODE1"
+
+
+@given("a Changed Event with invalid ODSCode is provided")
+def a_change_event_with_invalid_odscode(context):
     """Creates a valid change event"""
     context.change_event = get_change_event_dict()
-    context.change_event["ODSCode"] = "FAKE5"
+    context.change_event["ODSCode"] = "FAKE6"
 
 
-@given("a change event with invalid OrganisationSubType is provided")
-def a_change_event_is_provided(context):
+@given("a Changed Event with invalid OrganisationSubType is provided")
+def a_change_event_with_invalid_organisationsubtype(context):
     """Creates a valid change event"""
     context.change_event = get_change_event_dict()
     context.change_event["OrganisationSubType"] = "com"
 
 
-@when("the change event is sent to the event processor")
-def the_change_event_is_processed(context):
+@given("a Changed Event with invalid OrganisationTypeID is provided")
+def a_change_event_with_invalid_organisationtypeid(context):
+    """Creates a valid change event"""
+    context.change_event = get_change_event_dict()
+    context.change_event["OrganisationTypeID"] = "DEN"
+
+
+@when("the Changed Event is sent for processing")
+def the_change_event_is_sent_for_processing(context):
+    debug_purge_queue()
     context.response = process_payload(context.change_event)
-    context.correlation_id = context.response.headers["x-amz-apigw-id"]
-    # debug_purge_queue()
+    context.correlation_id = context.response.headers["x-amzn-RequestId"]
+    context.sequence_no = context.response.request.headers["sequence-number"]
+    print(context.sequence_no)
     message = context.response.json()
     assert (
         context.response.status_code == 200
     ), f"Status code not as expected.. Status code: {context.response.status_code} Error: {message}"
 
 
-@when('a "{valid}" change event with sequence id "{corrid}" is sent to the event procesor')
+@when('a "{valid}" Change Event with sequence id "{corrid}" is sent to the event procesor')
 def a_valid_change_event_with_seq_id_is_processed(context, valid: str, corrid: str):
     response = process_payload(valid, corrid)
     message = response.json()
@@ -53,7 +70,7 @@ def a_valid_change_event_with_seq_id_is_processed(context, valid: str, corrid: s
     ), f"Status code not as expected.. Status code: {response.status_code} Error: {message}"
 
 
-@when('an "{invalid}" change event with sequence id "{corrid}" is processed')
+@when('an "{invalid}" Change Event with sequence id "{corrid}" is processed')
 def an_invalid_change_event_with_seq_id_is_processed(context, invalid: str, corrid: str):
     response = process_payload(invalid, corrid)
     message = response.json()
@@ -63,31 +80,58 @@ def an_invalid_change_event_with_seq_id_is_processed(context, invalid: str, corr
     debug_purge_queue()
 
 
-@when('an "{expected}" change event is sent to the event procesor')
+@when('an "{expected}" Change Event is sent to the event procesor')
 def an_expected_change_event_is_sent_to_the_event_procesor(context, expected: str):
     response = get_response(expected)
     assert response == "Change event received", f"ERROR! Invalid Payload received.."
 
 
-@then("the processor lambda logs are generated")
-def the_processor_lambda_logs_are_generated(context):
+@then('the "{event}" logs are generated')
+def the_lambda_logs_are_generated(context, event: str):
     query = f'fields message | sort @timestamp asc | filter correlation_id="{context.correlation_id}" | filter message="Event has been validated"'
-    logs = get_logs(query)
-    assert logs != [], f"ERROR!! No logs found!.."
+    event_logs = get_logs(query, event)
+    assert event_logs != [], f"ERROR!! No logs found!.."
+    print(event_logs)
 
 
-@then('the "{valid}" payload with sequence id "{seqid}" is stored in dynamo db')
-def stored_dynamo_db_events_are_pulled(context, seqid: str, valid: str):
-    db_event_record = get_stored_events_from_db(seqid)
-    expected_payload = json.loads(get_payload(valid))
-    assert db_event_record != [], f"ERROR!! Event record NOT found!.."
-    assert expected_payload["ODSCode"] == db_event_record["ODSCode"], f"ERROR!!.. Change event record mismatch!!"
-    assert (
-        expected_payload["SequenceNumber"] == db_event_record["SequenceNumber"]
-    ), f"ERROR!!.. Change event record mismatch!!"
+@then("the Changed Event is stored in dynamo db")
+def stored_dynamo_db_events_are_pulled(context):
+    odscode = context.change_event["ODSCode"]
+    sequence_num = Decimal(context.sequence_no)
+    db_event_record = get_stored_events_from_db(odscode, sequence_num)
+    assert db_event_record is not None, f"ERROR!! Event record with odscode {odscode} NOT found!.."
+    assert odscode == db_event_record["ODSCode"], f"ERROR!!.. Change event record(odscode) mismatch!!"
+    assert sequence_num == db_event_record["SequenceNumber"], f"ERROR!!.. Change event record(sequence no) mismatch!!"
 
 
 @then("the lambda is confirmed active")
 def the_lambda_is_confirmed_active(context):
     status = get_lambda_info("status")
     assert status == "Active" or "Successful", f"ERROR! Invocation either unsuccessful or Lambda is inactive"
+
+
+@then("the Changed Event is processed")
+def the_changed_event_is_processed(context):
+    query = f'fields message | sort @timestamp asc | filter correlation_id="{context.correlation_id}" | filter message like "change request"'
+    logs = get_logs(query, "processor")
+    print(logs)
+    assert "DoS Integration CR" in logs, f"ERROR!!.. logs not found"
+
+
+@then("the unmatched service exception is reported to cloudwatch")
+def unmatched_service_exception(context):
+    query = f'fields message | sort @timestamp asc | filter correlation_id="{context.correlation_id}" | filter message like "No matching DOS services"'
+    logs = get_logs(query, "processor")
+    odscode = context.change_event["ODSCode"]
+    assert f"ODSCode '{odscode}'" in logs, f"ERROR!!.. expected unmatched service logs not found."
+
+@then("the processed Changed Request is sent to Dos")
+def processed_changed_event_sent_to_dos(context):
+    query = f'fields message | sort @timestamp asc | filter correlation_id="{context.correlation_id}" | filter message like "Successfully send change request to DoS"'
+    logs = get_logs(query, "sender")
+
+# The below step definition answers to a corresponding 'AND' annotation within the feature file
+@then("the Changed Request is accepted by Dos")
+def the_changed_request_is_accepted_by_dos(context):
+    """assert dos API response and validate processed record in Dos CR Queue database"""
+    pass
--- test/integration/features/utilities/utils.py
+++ test/integration/features/utilities/utils.py
@@ -7,7 +7,7 @@ from boto3 import client
 from time import sleep
 from decimal import Decimal
 import boto3
-from boto3.dynamodb.conditions import Attr
+from boto3.dynamodb.conditions import Attr, Key
 
 url = getenv("URL")
 SQS_URL = getenv("SQS_URL")
@@ -20,13 +20,19 @@ dynamodb = boto3.resource("dynamodb")
 
 
 def process_payload(payload: dict) -> str:
+    sequence_no = generate_unique_int(payload["ODSCode"])
+    # sequence_no = str(get_latest_sequence_id_for_a_given_odscode(payload["ODSCode"]))
+    print(sequence_no)
+    # sequence_no = "50"
     headers = {
         "x-api-key": json.loads(get_secret())[getenv("NHS_UK_API_KEY")],
-        "sequence-number": str(get_new_sequence_id_for_a_given_odscode(payload["ODSCode"])),
+        "sequence-number": sequence_no,
         "Content-Type": "application/json",
     }
+    payload["Address1"] = sequence_no+" MANSFIELD ROAD"
+    print(payload["Address1"])
     output = requests.request("POST", url, headers=headers, data=dumps(payload))
-    sleep(30)
+    sleep(5)
     return output
 
 
@@ -37,26 +43,51 @@ def debug_purge_queue():
         print(f"ERROR!..UNABLE TO PURGE. {e}")
 
 
-def get_stored_events_from_db(seq_id: str) -> dict:
+def get_stored_events_from_db(odscode: str, sequence_number: Decimal) -> dict:
     table = dynamodb.Table(dynamo_db_table)
-    sequence_number = seq_id
-    response = table.scan(FilterExpression=Attr("SequenceNumber").eq(Decimal(sequence_number)))
-    items = response["Items"]
-    for item in items:
-        return item
+    response = table.scan(
+        FilterExpression=Attr("ODSCode").eq(odscode)& Attr("SequenceNumber").eq(sequence_number)
+    )
+    #  & Attr("SequenceNumber").eq(Decimal(sequence_number))
+    item = response["Items"][0]
+    # for item in items:
+    return item
     # return json.loads(items, indent=2, default=str)
 
+# def get_dynamo_record(odscode: str, sequence_no: str) -> int:
+#     # Get latest sequence id for a given odscode from dynamodb
+#     try:
+#         table = dynamodb.Table(dynamo_db_table)
+#         resp = table.query(
+#         KeyConditionExpression=Key("ODSCode").eq(odscode) & Key("SequenceNumber").eq(Decimal(sequence_no)
+#         )
+    #     resp = dynamo_client.query(
+    #         TableName=dynamo_db_table,
+    #         IndexName="gsi_ods_sequence",
+    #         KeyConditionExpression=("ODSCode = :odscode", "SequenceNumber = :sequencenumber"),
+    #         ExpressionAttributeValues={
+    #             ":odscode": {"S": odscode}, ":sequencenumber": {"N": sequence_no}
+    #         },
+    # KeyConditionExpression=Key('ODSCode').eq(odscode) & Key('SequenceNumber').eq(sequence_no)
 
-def get_response(payload: str, corr_id: str) -> str:
-    payload_name = payload
-    correlation_id = corr_id
-    response = process_change_event(payload_name, correlation_id)
+    #         Limit=1,
+    #         ScanIndexForward=False,
+    #         ProjectionExpression="ODSCode,SequenceNumber,Event",
+    #     )
+    #     sequence_number = 0
+    #     if resp.get("Count") > 0:
+    #         sequence_number = int(resp.get("Items")[0]["SequenceNumber"]["N"])
+    # except Exception as err:
+    #     print(f"Unable to get sequence id from dynamodb for a given ODSCode {odscode} .Error: {err}")
+    #     raise
+    # return resp
+
+def get_response(payload: str) -> str:
+    response = process_payload(payload)
     return response.json()["message"]
 
 
 # This matches a payload file with a string describing it from the Steps
-
-
 def get_payload(payload_name: str) -> str:
     values = {"valid": "11_expected_schema.json", "invalid": "10_invalid.json"}
     if payload_name in ["valid", "invalid"]:
@@ -74,7 +105,7 @@ def get_lambda_info(info_param: str) -> str:
     return response["Configuration"][param]
 
 
-def get_new_sequence_id_for_a_given_odscode(odscode: str) -> int:
+def get_latest_sequence_id_for_a_given_odscode(odscode: str) -> int:
     # Get latest sequence id for a given odscode from dynamodb
     try:
         resp = dynamo_client.query(
@@ -94,4 +125,7 @@ def get_new_sequence_id_for_a_given_odscode(odscode: str) -> int:
     except Exception as err:
         print(f"Unable to get sequence id from dynamodb for a given ODSCode {odscode} .Error: {err}")
         raise
-    return sequence_number + 1
+    return sequence_number
+
+def generate_unique_int(odscode: str)-> str:
+    return str(get_latest_sequence_id_for_a_given_odscode(odscode)+1)
