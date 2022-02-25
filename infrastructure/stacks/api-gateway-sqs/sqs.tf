f5ea8ce808cfd9e9039c7caa084651336420db96
SLClayton <SLClayton@users.noreply.github.com>
2022-02-25 12:55:56 +0000

    Added in DLQs

--- infrastructure/stacks/api-gateway-sqs/sqs.tf
+++ infrastructure/stacks/api-gateway-sqs/sqs.tf
@@ -43,14 +43,14 @@ resource "aws_lambda_event_source_mapping" "fifo_event_source_mapping" {
 resource "aws_sqs_queue" "di_dead_letter_queue_from_fifo_queue" {
   name                      = var.dead_letter_queue_from_fifo_queue_name
   fifo_queue                = true
-  sqs_managed_sse_enabled   = true
+  kms_master_key_id         = data.aws_kms_key.signing_key.key_id
   message_retention_seconds = 1209600 # 14 days
 }
 
 resource "aws_sqs_queue" "cr_di_dead_letter_queue_from_fifo_queue" {
   name                      = var.cr_dead_letter_queue_from_fifo_queue_name
   fifo_queue                = true
-  sqs_managed_sse_enabled   = true
+  kms_master_key_id         = data.aws_kms_key.signing_key.key_id
   message_retention_seconds = 1209600 # 14 days
 }
 
