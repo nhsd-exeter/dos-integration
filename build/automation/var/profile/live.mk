9faa32d6854b62f43e9fc7ed6af84a03591705fa
Ed Sparkes <1023491+ed-sparkes@users.noreply.github.com>
2022-03-03 14:27:26 +0000

    Prod make files

--- build/automation/var/profile/live.mk
+++ build/automation/var/profile/live.mk
@@ -1,8 +1,20 @@
 -include $(VAR_DIR)/profile/prod.mk
 
-# API Gateway Route53
-TF_VAR_dos_integration_sub_domain_name := $(PROJECT_ID)-$(ENVIRONMENT)
+DOS_API_GATEWAY_SECRETS = core-dos/deployment
+DOS_API_GATEWAY_USERNAME_KEY := DOS_API_GATEWAY_USER
+DOS_API_GATEWAY_PASSWORD_KEY := DOS_API_GATEWAY_PASSWORD
+DOS_API_GATEWAY_URL := https://api-gateway.directoryofservices.nhs.uk/api/change-request
 
-# API Gateway Route53
-TF_VAR_dos_integration_sub_domain_name := $(PROGRAMME)-$(TEAM_ID)-$(ENVIRONMENT)
-DOS_INTEGRATION_URL := $(TF_VAR_dos_integration_sub_domain_name).$(TEXAS_HOSTED_ZONE)
+MOCK_MODE := false
+
+
+DB_SERVER_NAME := uec-core-dos-live-db-12-replica-di
+DB_PORT := 5432
+DB_NAME := pathwaysdos
+DB_SCHEMA := pathwaysdos
+DB_SECRET_NAME := core-dos/deployment
+DB_SECRET_KEY := DB_DI_READONLY_PASSWORD
+DB_USER_NAME_SECRET_NAME = uec-dos-int-live/deployment
+DB_USER_NAME_SECRET_KEY = DOS_REPLICA_DI_USERNAME
+
+TF_VAR_ip_address_secret := uec-dos-int-live-ip-addresses-allowlist
