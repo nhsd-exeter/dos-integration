f248ae0161201e3c1503c8a9a6a1a573a72e487b
Jack Plowman <jack.plowman1@nhs.net>
2022-03-10 10:32:20 +0000

    Remove live.mk changes

--- build/automation/var/profile/live.mk
+++ build/automation/var/profile/live.mk
@@ -5,10 +5,6 @@ DOS_API_GATEWAY_USERNAME_KEY := DOS_API_GATEWAY_USER
 DOS_API_GATEWAY_PASSWORD_KEY := DOS_API_GATEWAY_PASSWORD
 DOS_API_GATEWAY_URL := https://api-gateway.directoryofservices.nhs.uk/api/change-request
 
-MOCK_MODE := false
-
-
-
 DB_SERVER_NAME := uec-core-dos-live-db-12-replica-di
 DB_PORT := 5432
 DB_NAME := pathwaysdos
@@ -16,17 +12,6 @@ DB_SCHEMA := pathwaysdos
 DB_SECRET_NAME := core-dos/deployment
 DB_SECRET_KEY := DB_DI_READONLY_PASSWORD
 DB_USER_NAME_SECRET_NAME = uec-dos-int-live/deployment
-DB_USER_NAME_SECRET_KEY = DOS_REPLICA_DI_USERNAME
-
-TF_VAR_ip_address_secret := uec-dos-int-live-ip-addresses-allowlist
-
-DB_SERVER_NAME := uec-core-dos-live-db-12-replica-di
-DB_PORT := 5432
-DB_NAME := pathwaysdos
-DB_SCHEMA := pathwaysdos
-DB_SECRET_NAME := core-dos/deployment
-DB_SECRET_KEY := DB_DI_READONLY_PASSWORD
-DB_USER_NAME_SECRET_NAME = uec-dos-int-$(PROFILE)/deployment
 DB_USER_NAME_SECRET_KEY = DOS_REPLICA_DI_USERNAME
 
 TF_VAR_ip_address_secret := uec-dos-int-live-ip-addresses-allowlist
