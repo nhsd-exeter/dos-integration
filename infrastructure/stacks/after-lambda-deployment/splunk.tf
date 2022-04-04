e041fa61366b3a83c0ff596961dce9332b0c923e
Jack Plowman <jack.plowman1@nhs.net>
2022-04-04 16:58:14 +0100

    Reduce chance of error due to log group not existing

--- infrastructure/stacks/after-lambda-deployment/splunk.tf
+++ infrastructure/stacks/after-lambda-deployment/splunk.tf
@@ -1,4 +1,5 @@
 resource "aws_cloudwatch_log_subscription_filter" "event_processor_logs_subscription_filter" {
+  count           = var.profile == "task" ? 0 : 1
   name            = var.event_processor_subscription_filter_name
   role_arn        = data.aws_iam_role.firehose_role.arn
   log_group_name  = "/aws/lambda/${var.event_processor_lambda_name}"
@@ -8,6 +9,7 @@ resource "aws_cloudwatch_log_subscription_filter" "event_processor_logs_subscrip
 }
 
 resource "aws_cloudwatch_log_subscription_filter" "event_sender_logs_subscription_filter" {
+  count           = var.profile == "task" ? 0 : 1
   name            = var.event_sender_subscription_filter_name
   role_arn        = data.aws_iam_role.firehose_role.arn
   log_group_name  = "/aws/lambda/${var.event_sender_lambda_name}"
@@ -17,6 +19,7 @@ resource "aws_cloudwatch_log_subscription_filter" "event_sender_logs_subscriptio
 }
 
 resource "aws_cloudwatch_log_subscription_filter" "orchestrator_logs_subscription_filter" {
+  count           = var.profile == "task" ? 0 : 1
   name            = var.orchestrator_subscription_filter_name
   role_arn        = data.aws_iam_role.firehose_role.arn
   log_group_name  = "/aws/lambda/${var.orchestrator_lambda_name}"
@@ -26,6 +29,7 @@ resource "aws_cloudwatch_log_subscription_filter" "orchestrator_logs_subscriptio
 }
 
 resource "aws_cloudwatch_log_subscription_filter" "fifo_dlq_handler_logs_subscription_filter" {
+  count           = var.profile == "task" ? 0 : 1
   name            = var.fifo_dlq_handler_subscription_filter_name
   role_arn        = data.aws_iam_role.firehose_role.arn
   log_group_name  = "/aws/lambda/${var.fifo_dlq_handler_lambda_name}"
@@ -35,6 +39,7 @@ resource "aws_cloudwatch_log_subscription_filter" "fifo_dlq_handler_logs_subscri
 }
 
 resource "aws_cloudwatch_log_subscription_filter" "cr_fifo_dlq_handler_logs_subscription_filter" {
+  count           = var.profile == "task" ? 0 : 1
   name            = var.cr_fifo_dlq_handler_subscription_filter_name
   role_arn        = data.aws_iam_role.firehose_role.arn
   log_group_name  = "/aws/lambda/${var.cr_fifo_dlq_handler_lambda_name}"
@@ -44,6 +49,7 @@ resource "aws_cloudwatch_log_subscription_filter" "cr_fifo_dlq_handler_logs_subs
 }
 
 resource "aws_cloudwatch_log_subscription_filter" "event_replay_logs_subscription_filter" {
+  count           = var.profile == "task" ? 0 : 1
   name            = var.event_replay_subscription_filter_name
   role_arn        = data.aws_iam_role.firehose_role.arn
   log_group_name  = "/aws/lambda/${var.event_replay_lambda_name}"
@@ -53,6 +59,7 @@ resource "aws_cloudwatch_log_subscription_filter" "event_replay_logs_subscriptio
 }
 
 resource "aws_cloudwatch_log_subscription_filter" "di_endpoint_access_logs" {
+  count           = var.profile == "task" ? 0 : 1
   name            = var.change_event_gateway_subscription_filter_name
   role_arn        = data.aws_iam_role.firehose_role.arn
   log_group_name  = "/aws/api-gateway/${var.di_endpoint_api_gateway_name}"
