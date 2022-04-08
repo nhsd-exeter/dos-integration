238fc46d02201b1534dddf8d4248b37d64767dcf
mik3ola <michael.olaw1@nhs.net>
2022-04-08 14:29:56 +0100

    Adjust query length on test db checker handler

--- application/test_db_checker_handler/test_db_checker_handler.py
+++ application/test_db_checker_handler/test_db_checker_handler.py
@@ -32,24 +32,24 @@ def lambda_handler(event: Dict[str, Any], context: LambdaContext) -> str:
     if request["type"] == "get_pharmacy_odscodes":
         type_id_query = get_valid_service_types_equals_string("PHA")
         query = (
-                f"SELECT LEFT(odscode, 5) FROM services WHERE typeid {type_id_query} "
-                f"AND statusid = {VALID_STATUS_ID} AND odscode IS NOT NULL"
-            )
+            f"SELECT LEFT(odscode, 5) FROM services WHERE typeid {type_id_query} "
+            f"AND statusid = {VALID_STATUS_ID} AND odscode IS NOT NULL"
+        )
         result = run_query(query, None)
     elif request["type"] == "get_single_pharmacy_service_odscode":
         type_id_query = get_valid_service_types_equals_string("PHA")
         query = (
-                f"SELECT LEFT(odscode,5) FROM services WHERE typeid = {type_id_query} "
-                f"AND statusid = {VALID_STATUS_ID} AND odscode IS NOT NULL AND RIGHT(address, 1) != '$' "
-                "AND LENGTH(LEFT(odscode,5)) = 5 GROUP BY LEFT(odscode,5) HAVING COUNT(LEFT(odscode,5)) = 1"
-            )
+            f"SELECT LEFT(odscode,5) FROM services WHERE typeid = {type_id_query} "
+            f"AND statusid = {VALID_STATUS_ID} AND odscode IS NOT NULL AND RIGHT(address, 1) != '$' "
+            "AND LENGTH(LEFT(odscode,5)) = 5 GROUP BY LEFT(odscode,5) HAVING COUNT(LEFT(odscode,5)) = 1"
+        )
         result = run_query(query, None)
     elif request["type"] == "get_dentist_odscodes":
         type_id_query = get_valid_service_types_equals_string("Dentist")
         query = (
-                f"SELECT odscode FROM services WHERE typeid {type_id_query} "
-                f"AND statusid = {VALID_STATUS_ID} AND odscode IS NOT NULL AND LENGTH(odscode) = 6 AND LEFT(odscode, 1) ='V'"
-            )
+            f"SELECT odscode FROM services WHERE typeid {type_id_query} "
+            f"AND statusid = {VALID_STATUS_ID} AND odscode IS NOT NULL AND LENGTH(odscode) = 6 AND LEFT(odscode, 1)='V'"
+        )
         result = run_query(query, None)
     elif request["type"] == "get_changes":
         cid = request.get("correlation_id")
