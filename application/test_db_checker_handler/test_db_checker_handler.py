b71c3a31713182bf4c1ea42d81f30cdea3e2bc8a
mik3ola <michael.olaw1@nhs.net>
2022-05-07 02:04:32 +0100

    Update db checker handler query syntax

--- application/test_db_checker_handler/test_db_checker_handler.py
+++ application/test_db_checker_handler/test_db_checker_handler.py
@@ -40,8 +40,8 @@ def lambda_handler(event: Dict[str, Any], context: LambdaContext) -> str:
         type_id_query = get_valid_service_types_equals_string("PHA")
         query = (
             f"SELECT LEFT(odscode,5) FROM services WHERE typeid {type_id_query} AND LENGTH(LEFT(odscode,5)) = 5 "
-            f"AND statusid = {VALID_STATUS_ID} AND odscode IS NOT NULL AND web != '' AND RIGHT(address, 1) != '$' "
-            "AND publicphone != '' GROUP BY LEFT(odscode,5) HAVING COUNT(LEFT(odscode,5)) = 1"
+            f"AND statusid = {VALID_STATUS_ID} AND odscode IS NOT NULL AND RIGHT(address, 1) != '$' "
+            "AND publicphone IS NOT NULL AND web IS NOT NULL GROUP BY LEFT(odscode,5) HAVING COUNT(LEFT(odscode,5)) = 1"
         )
         result = run_query(query, None)
     elif request["type"] == "get_single_pharmacy_service_odscode":
