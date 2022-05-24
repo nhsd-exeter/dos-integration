a55a9429ba30edf9439910b1d3f540fd13c4d8f7
mik3ola <michael.olaw1@nhs.net>
2022-05-24 19:52:06 +0100

    Update unit test

--- application/test_db_checker_handler/tests/test_test_db_checker_handler.py
+++ application/test_db_checker_handler/tests/test_test_db_checker_handler.py
@@ -47,8 +47,8 @@ def test_type_get_single_service_pharmacy_odscode(mock_run_query, lambda_context
     # Assert
     mock_run_query.assert_called_once_with(
         "SELECT LEFT(odscode,5) FROM services WHERE typeid IN (13, 131, 132, 134, 137) "
-        "AND statusid = 1 AND odscode IS NOT NULL AND RIGHT(address, 1) != '$' AND LENGTH(LEFT(odscode,5)) = 5 "
-        "GROUP BY LEFT(odscode,5) HAVING COUNT(LEFT(odscode,5)) = 1",
+        "AND statusid = 1 AND odscode IS NOT NULL AND RIGHT(address, 1) != '$' "
+        "AND LENGTH(odscode) > 4 GROUP BY LEFT(odscode,5) HAVING COUNT(odscode) = 1",
         None,
     )
     assert response == '["ODS12", "ODS11"]'
@@ -63,9 +63,9 @@ def test_type_get_pharmacy_odscodes_with_contacts(mock_run_query, lambda_context
     response = lambda_handler(test_input, lambda_context)
     # Assert
     mock_run_query.assert_called_once_with(
-        "SELECT LEFT(odscode,5) FROM services WHERE typeid IN (13, 131, 132, 134, 137) AND LENGTH(LEFT(odscode,5)) = 5 "
+        "SELECT LEFT(odscode,5) FROM services WHERE typeid IN (13, 131, 132, 134, 137) AND LENGTH(odscode) > 4 "
         "AND statusid = 1 AND odscode IS NOT NULL AND RIGHT(address, 1) != '$' "
-        "AND publicphone IS NOT NULL AND web IS NOT NULL GROUP BY LEFT(odscode,5) HAVING COUNT(LEFT(odscode,5)) = 1",
+        "AND publicphone IS NOT NULL AND web IS NOT NULL GROUP BY LEFT(odscode,5) HAVING COUNT(odscode) = 1",
         None,
     )
     assert response == '["ODS12", "ODS11"]'
