b828dc58dd0cc2481db9cd88f7a35eb794aecf86
Jack Plowman <jack.plowman1@nhs.net>
2022-06-17 09:29:05 +0100

    Remove unused data source

--- infrastructure/stacks/perf-test-tools/data.tf
+++ infrastructure/stacks/perf-test-tools/data.tf
@@ -1,12 +1,3 @@
-data "terraform_remote_state" "development_pipeline" {
-  backend = "s3"
-  config = {
-    bucket = var.service_state_bucket
-    key    = var.development_pipeline_state
-    region = var.aws_region
-  }
-}
-
 data "template_file" "perf_buildspec" {
   template = file("perf-buildspec.yml")
 }
