519d2c252f1157410da0192f21adc86af025500e
Jack Plowman <62281988+JackPlowman@users.noreply.github.com>
2022-09-27 10:05:30 +0100

    DI-523 Update Production profiles for release 3.0 (#530)
    Add Production profiles for release 3.0

--- build/automation/var/profile/live.mk
+++ build/automation/var/profile/live.mk
@@ -7,11 +7,15 @@ LOG_LEVEL:= INFO
 
 DB_SERVER_NAME := uec-core-dos-live-db-12
 DB_REPLICA_SERVER_NAME := uec-core-dos-live-db-12-replica-di
-
 DB_PORT := 5432
 DB_NAME := pathwaysdos
 DB_SCHEMA := pathwaysdos
+
 DB_SECRET_NAME := core-dos/deployment
+DB_SECRET_KEY := DB_DI_READWRITE_PASSWORD
+DB_USER_NAME_SECRET_NAME = uec-dos-int-live/deployment
+DB_USER_NAME_SECRET_KEY = DOS_DB_DI_USERNAME
+
 DB_REPLICA_SECRET_NAME := core-dos/deployment
 DB_REPLICA_SECRET_KEY := DB_DI_READONLY_PASSWORD
 DB_READ_ONLY_USER_NAME_SECRET_NAME = uec-dos-int-live/deployment
