b459865d51d340624c7c6e3d58f589d203e058dd
Jack Plowman <jack.plowman1@nhs.net>
2022-10-03 08:26:48 +0100

    Confirm same tags are used in Terraform and Serverless Framework

--- infrastructure/stacks/common/common-provider.tf
+++ infrastructure/stacks/common/common-provider.tf
@@ -2,19 +2,19 @@ provider "aws" {
   region = var.aws_region
   default_tags {
     tags = {
-      "Profile"            = var.profile
-      "Owner"              = var.distribution_list
-      "TagVersion"         = "2.0"
       "DataClassification" = var.data_classification
-      "Programme"          = var.programme
-      "Service"            = var.project_id
-      "Product"            = var.project_id
       "Environment"        = var.environment
-      "Environment_Type"   = var.aws_account_name
-      "PublicFacing"       = "No"
-      "ServiceCategory"    = var.service_category
-      "Tool"               = "Terraform"
+      "EnvironmentType"    = var.aws_account_name
+      "Owner"              = var.distribution_list
+      "Product"            = var.project_id
+      "Profile"            = var.profile
+      "Programme"          = var.programme
       "Project"            = var.project_display_name
+      "PublicFacing"       = "No"
+      "Service"            = var.project_id
+      "ServiceCategory"    = var.service_category
+      "TagVersion"         = "2.0"
+      "Tool"               = "Terraform"
     }
   }
 }
