ae48e26ef6f72211911ec40c2e52fdb6b81b184f
SLClayton <SLClayton@users.noreply.github.com>
2022-10-07 16:20:46 +0100

    Fixing unit tests in code

---
+++ application/comparison_reporting/tests/test_run_update_error_reports.py
@@ -0,0 +1,9 @@
+from datetime import date
+from unittest.mock import patch
+
+from comparison_reporting.run_update_error_reports import run_update_error_reports
+
+
+def run_update_error_reports(mock_download_csv_as_dicts):
+    pass
+
