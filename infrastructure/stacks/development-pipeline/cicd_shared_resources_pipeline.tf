f392fbba0967f8f5f5a852200e91040239e77932
Jack Plowman <62281988+JackPlowman@users.noreply.github.com>
2022-11-11 10:38:33 +0000

    DI-619 Add codebuild stage to trigger shared resources pipeline (#573)
    Add codebuild stage to trigger shared resources pipeline

--- infrastructure/stacks/development-pipeline/cicd_shared_resources_pipeline.tf
+++ infrastructure/stacks/development-pipeline/cicd_shared_resources_pipeline.tf
@@ -1,6 +1,6 @@
-module "cicd_pipeline_artefact_bucket" {
+module "cicd_shared_resources_pipeline_artefact_bucket" {
   source             = "../../modules/s3"
-  name               = "${var.project_id}-${var.environment}-cicd-pipeline-artefacts"
+  name               = "${var.project_id}-${var.environment}-cicd-shared-resources-pipeline-artefact"
   project_id         = var.project_id
   acl                = "private"
   versioning_enabled = "true"
