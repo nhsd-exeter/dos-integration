fe5a09d80ba5cab19cf6312e87ef151b1af73288
Kit <christopher.corbishley1@nhs.net>
2022-11-16 16:54:53 +0000

    Commit to change branches

--- application/dos_db_handler/dos_db_handler.py
+++ application/dos_db_handler/dos_db_handler.py
@@ -96,7 +96,7 @@ def lambda_handler(event: Dict[str, Any], context: LambdaContext) -> str:
             raise ValueError(f"No matching services for odscode {odscode}")
         query_results = query_results[0]
         result = dict(zip(db_columns, query_results))
-        #a difference
+        # a difference
         return result
     else:
         raise ValueError("Unsupported request")
