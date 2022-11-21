27e4185261a3362f253114f656bb929d870a67a6
Jack Plowman <jack.plowman1@nhs.net>
2022-11-21 14:03:27 +0000

    Fix Checkov failed checks

--- infrastructure/stacks/blue-green-link/parameter_store.tf
+++ infrastructure/stacks/blue-green-link/parameter_store.tf
@@ -1,10 +1,12 @@
 resource "aws_ssm_parameter" "blue_green_deployment_previous_version" {
+  #checkov:skip=CKV2_AWS_34:Value does not contain sensitive data so it is ok to be stored in plain text
   name  = var.blue_green_deployment_previous_version_parameter_name
   type  = "String"
   value = var.previous_blue_green_environment
 }
 
 resource "aws_ssm_parameter" "blue_green_deployment_current_version" {
+  #checkov:skip=CKV2_AWS_34:Value does not contain sensitive data so it is ok to be stored in plain text
   name  = var.blue_green_deployment_current_version_parameter_name
   type  = "String"
   value = var.blue_green_environment
