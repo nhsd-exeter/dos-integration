4a4c3b312198110d20f745edfa25053cac9ad66c
Matthew Begley <matthewbegley@nhs.net>
2023-01-20 16:56:41 +0000

    Fix - Add import for Dict and Any typing

--- application/ingest_change_event/ingest_change_event.py
+++ application/ingest_change_event/ingest_change_event.py
@@ -1,6 +1,7 @@
 from json import dumps
 from os import environ
 from time import gmtime, strftime, time_ns
+from typing import Any, Dict
 
 from aws_embedded_metrics import metric_scope
 from aws_lambda_powertools.logging import Logger
