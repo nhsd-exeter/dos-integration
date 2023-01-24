29cd9a0f032800e9177a970a767bf69a9cd00b46
Jack Plowman <62281988+JackPlowman@users.noreply.github.com>
2023-01-24 09:40:33 +0000

    DSUEC-58 Fix pre-release pipeline (#604)
    # Task Branch Pull Request
    
    **<https://nhsd-jira.digital.nhs.uk/browse/DSUEC-58>**
    
    ## Description of Changes
    
    This PR fixes the pre-release pipeline deployment when a release branch is created.

--- infrastructure/stacks/release-pipeline/variables.tf
+++ infrastructure/stacks/release-pipeline/variables.tf
@@ -31,13 +31,3 @@ variable "github_repo" {
   type        = string
   description = ""
 }
-
-variable "service_state_bucket" {
-  type        = string
-  description = "The S3 bucket used to store the service state"
-}
-
-variable "development_pipeline_state" {
-  type        = string
-  description = "Location of the Terraform state file for the development pipeline"
-}
