a53e4f7cef660062e524e07b4a56e1ad69b47dc8
Jack Plowman <jack.plowman1@nhs.net>
2023-02-27 14:12:50 +0000

    Add temporary nonprod fix

--- infrastructure/stacks/before-lambda-deployment/lambda_security_groups.tf
+++ infrastructure/stacks/before-lambda-deployment/lambda_security_groups.tf
@@ -37,7 +37,7 @@ resource "aws_security_group_rule" "database_allow_in_from_lambda" {
   protocol                 = "tcp"
   source_security_group_id = aws_security_group.uec_dos_int_lambda_sg.id
   security_group_id        = data.aws_security_group.dos_db_sg.id
-  description              = "Allow access in from UEC DI lambda to DoS Postgres DB"
+  description              = "Allow access in from UEC DI lambda to DoS Postgres DB - ${var.environment}}"
 }
 
 resource "aws_security_group_rule" "database_replica_allow_in_from_lambda" {
