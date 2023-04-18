96fd008e51235573d5dfd4139d54cebcf6d81126
Jack Plowman <62281988+JackPlowman@users.noreply.github.com>
2023-04-18 09:49:34 +0100

    DS-638 Add Object Encryption to File Upload (#668)
    # Task Branch Pull Request
    
    **<https://nhsd-jira.digital.nhs.uk/browse/DS-638>**
    
    ## Description of Changes
    
    This PR encrypts objects uploaded to the S3 send email bucket.

--- application/common/s3.py
+++ application/common/s3.py
@@ -14,7 +14,7 @@ def put_content_to_s3(content: bytes, s3_filename: str) -> None:
         s3_file_name (str): The filename when the file is stored in S3
     """
     bucket = getenv("SEND_EMAIL_BUCKET_NAME")
-    client("s3").put_object(Body=content, Bucket=bucket, Key=s3_filename)
+    client("s3").put_object(Body=content, Bucket=bucket, Key=s3_filename, ServerSideEncryption="AES256")
     logger.info(
         f"Uploaded to S3 as {s3_filename}",
         extra={"bucket": bucket, "s3_filename": s3_filename},
