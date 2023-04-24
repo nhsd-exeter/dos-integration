0c2d751085f8261bbdda0f79ef564a88a33ce837
Jack Plowman <62281988+JackPlowman@users.noreply.github.com>
2023-04-24 11:48:03 +0100

    DI-706 Disable Logging Bucket (#674)
    # Task Branch Pull Request
    
    **<https://nhsd-jira.digital.nhs.uk/browse/DS-705>**
    
    ## Description of Changes
    
    This PR removes the Server Access Logging bucket to remove the issue
    with ACLs. This feature will be restored after release 12.

--- infrastructure/stacks/shared-resources/s3.tf
+++ infrastructure/stacks/shared-resources/s3.tf
@@ -6,10 +6,10 @@ module "di_send_email_bucket" {
   force_destroy      = "true"
   object_ownership   = "ObjectWriter"
 
-  logging = {
-    target_bucket = module.di_logs_bucket.s3_bucket_id
-    target_prefix = "s3/send_email_bucket/"
-  }
+  # logging = {
+  #   target_bucket = module.di_logs_bucket.s3_bucket_id
+  #   target_prefix = "s3/send_email_bucket/"
+  # }
 
   server_side_encryption_configuration = {
     rule = [{
@@ -27,24 +27,24 @@ module "di_send_email_bucket" {
   ]
 }
 
-module "di_logs_bucket" {
-  source                   = "../../modules/s3"
-  name                     = var.logs_bucket_name
-  project_id               = var.project_id
-  acl                      = "log-delivery-write"
-  versioning_enabled       = "true"
-  force_destroy            = "true"
-  control_object_ownership = true
-  object_ownership         = "BucketOwnerPreferred"
+# module "di_logs_bucket" {
+#   source                   = "../../modules/s3"
+#   name                     = var.logs_bucket_name
+#   project_id               = var.project_id
+#   acl                      = "log-delivery-write"
+#   versioning_enabled       = "true"
+#   force_destroy            = "true"
+#   control_object_ownership = true
+#   object_ownership         = "BucketOwnerPreferred"
 
-  attach_policy = true
-  policy = jsonencode(
-    {
-      Action = "s3:PutObject"
-      Effect = "Allow"
-      Principal = {
-        Service = "logging.s3.amazonaws.com"
-      }
-      Resource = "arn:aws:s3:::${var.logs_bucket_name}/*"
-  })
-}
+#   attach_policy = true
+#   policy = jsonencode(
+#     {
+#       Action = "s3:PutObject"
+#       Effect = "Allow"
+#       Principal = {
+#         Service = "logging.s3.amazonaws.com"
+#       }
+#       Resource = "arn:aws:s3:::${var.logs_bucket_name}/*"
+#   })
+# }
