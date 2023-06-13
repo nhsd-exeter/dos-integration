2bd97f2cb7fa83035bebc31cca7f01db9270f8d1
Jack Plowman <jack.plowman1@nhs.net>
2023-06-13 15:13:18 +0100

    Scale lambda concurrency based on profile

--- build/automation/var/profile/demo.mk
+++ build/automation/var/profile/demo.mk
@@ -37,3 +37,9 @@ TAG_SECRET_MANAGER := uec-dos-int-live/deployment
 # Organisation Types Feature Flags
 
 ACCEPTED_ORG_TYPES := PHA
+
+# ==============================================================================
+# Performance variables
+
+SERVICE_MATCHER_MAX_CONCURRENCY := 80
+SERVICE_SYNC_MAX_CONCURRENCY := 50
