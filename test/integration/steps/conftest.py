b4f49bac47b9be4cfe9d2315c1458fd607451035
Jack Plowman <jack.plowman1@nhs.net>
2023-09-01 11:59:53 +0100

    Add Contraception integration tests

--- test/integration/steps/conftest.py
+++ test/integration/steps/conftest.py
@@ -1,7 +1,12 @@
 import pytest
 
 from .functions.context import Context
-from .functions.generator import set_up_common_condition_service_types, set_up_palliative_care_in_db
+from .functions.dos.set_up_data import (
+    set_up_blood_pressure_z_code_in_db,
+    set_up_common_condition_service_types,
+    set_up_contraception_z_code_in_db,
+    set_up_palliative_care_in_db,
+)
 
 
 def log_test_summary(step, request) -> None:
@@ -36,4 +41,6 @@ def context() -> Context:
 def pytest_sessionstart(session) -> None:
     """Called after the Session object has been created and before performing collection and entering the run test loop."""  # noqa: E501
     set_up_palliative_care_in_db()
+    set_up_blood_pressure_z_code_in_db()
+    set_up_contraception_z_code_in_db()
     set_up_common_condition_service_types()
