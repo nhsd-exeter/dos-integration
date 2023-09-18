923895ec5f3e4e2894938f72d46238bbf9a79cd7
Jack Plowman <jack.plowman1@nhs.net>
2023-09-18 11:44:43 +0100

    Add palliative care performance test

--- test/performance/functions/performance_test.py
+++ test/performance/functions/performance_test.py
@@ -73,14 +73,17 @@ class PerformanceTest(FastHttpUser):
         self.payload = change_event.create_change_event_json()
         send_change_event(request_name="OpeningTimesChangeEvent", request=self, valid_ods_code=True)
 
-    # Palliative care is not currently supported in the performance environments
-    # @task
-    # def palliative_care_changes_change_event(self: Self) -> None:
-    #     """Send a palliative care change event.
+    @task
+    def palliative_care_changes_change_event(self: Self) -> None:
+        """Send a palliative care change event.
 
-    #     Args:
-    #         self (Self): The class
-    #     """
+        Args:
+            self (Self): The class
+        """
+        change_event = ChangeEvent()
+        change_event.cause_palliative_care_updates()
+        self.payload = change_event.create_change_event_json()
+        send_change_event(request_name="PalliativeCareChangeEvent", request=self, valid_ods_code=True)
 
     @task
     def blood_pressure_updates_change_event(self: Self) -> None:
