5182a5c4403776d6830f176d2454793f67323fe8
Jack Plowman <62281988+JackPlowman@users.noreply.github.com>
2023-09-21 10:27:26 +0100

    DS-1417 Bump Up Pipeline Docker Image Versions (#850)
    # Task Branch Pull Request
    
    **<https://nhsd-jira.digital.nhs.uk/browse/DS-1417>**
    
    ## Description of Changes
    
    This PR bumps up the pipeline docker image versions.

--- infrastructure/stacks/development-and-deployment-tools/delete_blue_green_environment.tf
+++ infrastructure/stacks/development-and-deployment-tools/delete_blue_green_environment.tf
@@ -12,7 +12,7 @@ resource "aws_codebuild_project" "di_delete_blue_green_environment" {
 
   environment {
     compute_type                = "BUILD_GENERAL1_SMALL"
-    image                       = "aws/codebuild/amazonlinux2-x86_64-standard:4.0"
+    image                       = "aws/codebuild/amazonlinux2-x86_64-standard:5.0"
     type                        = "LINUX_CONTAINER"
     image_pull_credentials_type = "CODEBUILD"
     privileged_mode             = true
