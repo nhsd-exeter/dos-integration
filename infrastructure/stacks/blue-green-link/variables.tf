f0acba3c545c6cdf316b76fef1a807fa703da4cd
Jack Plowman <jack.plowman1@nhs.net>
2023-09-22 09:04:55 +0100

    Merge cloudwatch dashboards

--- infrastructure/stacks/blue-green-link/variables.tf
+++ infrastructure/stacks/blue-green-link/variables.tf
@@ -50,11 +50,6 @@ variable "cloudwatch_monitoring_dashboard_name" {
   description = "Name of the dashboard to see the various performance metrics in AWS"
 }
 
-variable "cloudwatch_data_dashboard_name" {
-  type        = string
-  description = "Name of the dashboard to see the various data metrics in AWS"
-}
-
 variable "dos_db_cluster_name" {
   type        = string
   description = "Name of db dos cluster to connect to"
