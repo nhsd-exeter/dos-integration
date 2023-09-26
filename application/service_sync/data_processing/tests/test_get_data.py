a3c23bf8356938566e7f575e6195cfe22e3a8ccd
Jack Plowman <jack.plowman1@nhs.net>
2023-09-26 13:32:10 +0100

    Fix rebase errors

--- application/service_sync/data_processing/tests/test_get_data.py
+++ application/service_sync/data_processing/tests/test_get_data.py
@@ -12,9 +12,9 @@ FILE_PATH = "application.service_sync.data_processing.get_data"
 @patch(f"{FILE_PATH}.get_standard_opening_times_from_db")
 @patch(f"{FILE_PATH}.DoSService")
 @patch(f"{FILE_PATH}.query_dos_db")
-@patch(f"{FILE_PATH}.connect_to_dos_db")
+@patch(f"{FILE_PATH}.connect_to_db_writer")
 def test_get_dos_service_and_history(
-    mock_connect_to_dos_db: MagicMock,
+    mock_connect_to_db_writer: MagicMock,
     mock_query_dos_db: MagicMock,
     mock_dos_service: MagicMock,
     mock_get_standard_opening_times_from_db: MagicMock,
@@ -29,24 +29,24 @@ def test_get_dos_service_and_history(
     # Assert
     assert mock_dos_service() == dos_service
     mock_get_standard_opening_times_from_db.assert_called_once_with(
-        connection=mock_connect_to_dos_db().__enter__(),
+        connection=mock_connect_to_db_writer().__enter__(),
         service_id=service_id,
     )
     mock_get_specified_opening_times_from_db.assert_called_once_with(
-        connection=mock_connect_to_dos_db().__enter__(),
+        connection=mock_connect_to_db_writer().__enter__(),
         service_id=service_id,
     )
     assert mock_service_histories() == service_history
     mock_service_histories.return_value.get_service_history_from_db.assert_called_once_with(
-        mock_connect_to_dos_db().__enter__(),
+        mock_connect_to_db_writer().__enter__(),
     )
     mock_service_histories.return_value.create_service_histories_entry.assert_called_once_with()
 
 
 @patch(f"{FILE_PATH}.query_dos_db")
-@patch(f"{FILE_PATH}.connect_to_dos_db")
+@patch(f"{FILE_PATH}.connect_to_db_writer")
 def test_get_dos_service_and_history_no_match(
-    mock_connect_to_dos_db: MagicMock,
+    mock_connect_to_db_writer: MagicMock,
     mock_query_dos_db: MagicMock,
 ):
     # Arrange
@@ -55,13 +55,13 @@ def test_get_dos_service_and_history_no_match(
     # Act
     with pytest.raises(ValueError, match=f"Service ID {service_id} not found"):
         get_dos_service_and_history(service_id)
-    mock_connect_to_dos_db.assert_called_once()
+    mock_connect_to_db_writer.assert_called_once()
 
 
 @patch(f"{FILE_PATH}.query_dos_db")
-@patch(f"{FILE_PATH}.connect_to_dos_db")
+@patch(f"{FILE_PATH}.connect_to_db_writer")
 def test_get_dos_service_and_history_mutiple_matches(
-    mock_connect_to_dos_db: MagicMock,
+    mock_connect_to_db_writer: MagicMock,
     mock_query_dos_db: MagicMock,
 ):
     # Arrange
@@ -70,4 +70,4 @@ def test_get_dos_service_and_history_mutiple_matches(
     # Act
     with pytest.raises(ValueError, match=f"Multiple services found for Service Id: {service_id}"):
         get_dos_service_and_history(service_id)
-    mock_connect_to_dos_db.assert_called_once()
+    mock_connect_to_db_writer.assert_called_once()
