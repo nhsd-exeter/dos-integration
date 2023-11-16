74fcd2b386a2582363da076200a9f11e5522e89d
Jack Plowman <62281988+JackPlowman@users.noreply.github.com>
2023-11-16 14:31:26 +0000

    DS-1724 Fix KMS Principals Being Set Incorrectly (#928)
    # Task Branch Pull Request
    
    **<https://nhsd-jira.digital.nhs.uk/browse/DS-1724>**
    
    ## Description of Changes
    
    This PR fixes the incorrect principals in the IAM policy for the KMS
    keys in the production account.

--- infrastructure/stacks/shared-resources/locals.tf
+++ infrastructure/stacks/shared-resources/locals.tf
@@ -4,5 +4,5 @@ locals {
   allowlist_ips_hash      = md5(join(",", local.ip_addresses))
   deployment_secrets      = jsondecode(data.aws_secretsmanager_secret_version.deployment_secrets.secret_string)
   aws_sso_role            = local.deployment_secrets["AWS_SSO_ROLE"]
-  aws_sso_read_write_role = var.aws_account_name != "prod" ? local.deployment_secrets["AWS_SSO_READ_WRITE_ROLE"] : ""
+  aws_sso_read_write_role = var.aws_account_name == "prod" ? local.deployment_secrets["AWS_SSO_READ_WRITE_ROLE"] : ""
 }
