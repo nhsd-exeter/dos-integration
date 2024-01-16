640eced1f47aad6a9fa23047ebb895984db7f7d1
Jack Plowman <62281988+JackPlowman@users.noreply.github.com>
2024-01-16 11:12:52 +0000

    DS-2064 Upgrade Development and Deployment Tools Terraform Provider (#984)
    # Task Branch Pull Request
    
    **<https://nhsd-jira.digital.nhs.uk/browse/DS-2064>**
    
    ## Description of Changes
    
    Update Development and Deployment Tools Terraform AWS provider version
    to 5.32.1

--- infrastructure/stacks/development-and-deployment-tools/terraform.tf
+++ infrastructure/stacks/development-and-deployment-tools/terraform.tf
@@ -5,7 +5,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.37.0"
+      version = "~> 5.32.1"
     }
     template = {
       source  = "hashicorp/template"
