e89094a544ea56f91fac4ab7bb613963036de43d
Sindhu <nsindhupriya@hotmail.co.uk>
2024-09-11 20:10:06 +0100

    Adding 403 forbidden gateway response

--- infrastructure/stacks/shared-resources/api-gateway-responses.tf
+++ infrastructure/stacks/shared-resources/api-gateway-responses.tf
@@ -191,7 +191,7 @@ resource "aws_api_gateway_gateway_response" "invalid_api_key_403_response" {
   rest_api_id        = aws_api_gateway_rest_api.di_endpoint.id
   status_code        = "403"
   response_type      = "INVALID_API_KEY"
-  response_templates = ({ "application/json" : jsonencode({ "message" : "Forbidden" }) })
+  response_templates = ({ "application/json" : jsonencode({ "message" : "Forbiddennn" }) })
 
   response_parameters = {
     "gatewayresponse.header.Cache-Control"             = "'no-cache'"
