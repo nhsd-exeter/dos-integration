f29e5d5fbbbf320c588e4498428645797cc2e131
Sindhu <nsindhupriya@hotmail.co.uk>
2024-09-12 08:59:15 +0100

    Adding dependency to api gateway

--- infrastructure/stacks/shared-resources/api-gateway.tf
+++ infrastructure/stacks/shared-resources/api-gateway.tf
@@ -95,7 +95,8 @@ EOF
 resource "aws_api_gateway_deployment" "di_endpoint_deployment" {
   rest_api_id = aws_api_gateway_rest_api.di_endpoint.id
   depends_on = [
-    aws_api_gateway_rest_api_policy.di_endpoint_policy
+    aws_api_gateway_rest_api_policy.di_endpoint_policy,
+    aws_api_gateway_gateway_response.invalid_api_key_403_response
   ]
   triggers = {
     redeployment = join("", [md5(jsonencode([
