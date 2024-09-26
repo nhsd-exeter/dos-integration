f7d9fcb46ae3e906771b017c21bcbc4629bca57b
Sindhu Natarajan <nsindhupriya@hotmail.co.uk>
2024-09-26 17:30:51 +0100

    DS-2829 Add missing HTTP security headers (#1027)
    # Task Branch Pull Request
    
    **<https://nhsd-jira.digital.nhs.uk/browse/DS-2829>**
    
    ## Description of Changes
    
    Adding missing HTTP security headers
    
    ---------
    
    Co-authored-by: Sindhu <nsindhu26@gmail.com>

--- infrastructure/stacks/shared-resources/api-gateway-responses.tf
+++ infrastructure/stacks/shared-resources/api-gateway-responses.tf
@@ -7,6 +7,9 @@ resource "aws_api_gateway_method_response" "response_200" {
     "method.response.header.Cache-control"             = true
     "method.response.header.Pragma"                    = true
     "method.response.header.Strict-Transport-Security" = true
+    "method.response.header.X-Frame-Options"           = true
+    "method.response.header.X-Content-Type-Options"    = true
+    "method.response.header.Content-Security-Policy"   = true
   }
   response_models = {
     "application/json" = aws_api_gateway_model.default_model.name
@@ -22,6 +25,9 @@ resource "aws_api_gateway_method_response" "response_400" {
     "method.response.header.Cache-control"             = true
     "method.response.header.Pragma"                    = true
     "method.response.header.Strict-Transport-Security" = true
+    "method.response.header.X-Frame-Options"           = true
+    "method.response.header.X-Content-Type-Options"    = true
+    "method.response.header.Content-Security-Policy"   = true
   }
   response_models = {
     "application/json" = aws_api_gateway_model.default_model.name
@@ -37,6 +43,9 @@ resource "aws_api_gateway_method_response" "response_500" {
     "method.response.header.Cache-control"             = true
     "method.response.header.Pragma"                    = true
     "method.response.header.Strict-Transport-Security" = true
+    "method.response.header.X-Frame-Options"           = true
+    "method.response.header.X-Content-Type-Options"    = true
+    "method.response.header.Content-Security-Policy"   = true
   }
   response_models = {
     "application/json" = aws_api_gateway_model.default_model.name
@@ -54,6 +63,9 @@ resource "aws_api_gateway_integration_response" "di_endpoint_integration_success
     "method.response.header.Cache-control"             = "'no-cache'"
     "method.response.header.Pragma"                    = "'no-store'"
     "method.response.header.Strict-Transport-Security" = "'max-age=31536000; includeSubDomains'"
+    "method.response.header.X-Frame-Options"           = "'DENY'"
+    "method.response.header.X-Content-Type-Options"    = "'nosniff'"
+    "method.response.header.Content-Security-Policy"   = "'default-src 'self''"
   }
 
   depends_on = [
@@ -75,6 +87,9 @@ resource "aws_api_gateway_integration_response" "response_400" {
     "method.response.header.Cache-control"             = "'no-cache'"
     "method.response.header.Pragma"                    = "'no-store'"
     "method.response.header.Strict-Transport-Security" = "'max-age=31536000; includeSubDomains'"
+    "method.response.header.X-Frame-Options"           = "'DENY'"
+    "method.response.header.X-Content-Type-Options"    = "'nosniff'"
+    "method.response.header.Content-Security-Policy"   = "'default-src 'self''"
   }
 
   depends_on = [
@@ -96,6 +111,9 @@ resource "aws_api_gateway_integration_response" "response_500" {
     "method.response.header.Cache-control"             = "'no-cache'"
     "method.response.header.Pragma"                    = "'no-store'"
     "method.response.header.Strict-Transport-Security" = "'max-age=31536000; includeSubDomains'"
+    "method.response.header.X-Frame-Options"           = "'DENY'"
+    "method.response.header.X-Content-Type-Options"    = "'nosniff'"
+    "method.response.header.Content-Security-Policy"   = "'default-src 'self''"
   }
 
   depends_on = [
@@ -112,9 +130,41 @@ resource "aws_api_gateway_gateway_response" "access_denied_403_gateway_response"
   response_type      = "ACCESS_DENIED"
   response_templates = ({ "application/json" : jsonencode({ "Message" : "Access Denied, please contact the development team for assistance" }) })
 
+  response_parameters = {
+    "gatewayresponse.header.Cache-control"             = "'no-cache'"
+    "gatewayresponse.header.Pragma"                    = "'no-store'"
+    "gatewayresponse.header.Strict-Transport-Security" = "'max-age=31536000; includeSubDomains'"
+    "gatewayresponse.header.X-Frame-Options"           = "'DENY'"
+    "gatewayresponse.header.X-Content-Type-Options"    = "'nosniff'"
+    "gatewayresponse.header.Content-Security-Policy"   = "'default-src 'self''"
+  }
+
   depends_on = [
     aws_api_gateway_integration.di_endpoint_integration,
     aws_api_gateway_resource.di_endpoint_change_event_path,
     aws_api_gateway_method.di_endpoint_method,
   ]
 }
+
+resource "aws_api_gateway_gateway_response" "invalid_api_key_403_response" {
+  rest_api_id        = aws_api_gateway_rest_api.di_endpoint.id
+  status_code        = "403"
+  response_type      = "INVALID_API_KEY"
+  response_templates = ({ "application/json" : jsonencode({ "message" : "Forbidden" }) })
+
+  response_parameters = {
+    "gatewayresponse.header.Cache-Control"             = "'no-cache'"
+    "gatewayresponse.header.Pragma"                    = "'no-store'"
+    "gatewayresponse.header.Strict-Transport-Security" = "'max-age=31536000; includeSubDomains'"
+    "gatewayresponse.header.X-Frame-Options"           = "'DENY'"
+    "gatewayresponse.header.X-Content-Type-Options"    = "'nosniff'"
+    "gatewayresponse.header.Content-Security-Policy"   = "'default-src 'self''"
+  }
+
+  depends_on = [
+    aws_api_gateway_integration.di_endpoint_integration,
+    aws_api_gateway_resource.di_endpoint_change_event_path,
+    aws_api_gateway_method.di_endpoint_method,
+  ]
+}
+
