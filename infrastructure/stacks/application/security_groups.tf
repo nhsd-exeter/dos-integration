8db933fa8e0b8a44298103f3554455293745f067
Thomas Cordall <thomas.cordall@kainos.com>
2024-10-11 11:18:12 +0100

    DS-2834 Restricted endpoint to security group

--- infrastructure/stacks/application/security_groups.tf
+++ infrastructure/stacks/application/security_groups.tf
@@ -11,24 +11,24 @@ resource "aws_security_group" "lambda_sg" {
 
 #tfsec:ignore:aws-vpc-no-public-egress-sgr
 resource "aws_security_group_rule" "allow_https_out" {
-  type              = "egress"
-  from_port         = 443
-  to_port           = 443
-  protocol          = "tcp"
-  cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.lambda_sg.id
-  description       = "Allow all HTTPS outbound traffic"
+  type                     = "egress"
+  from_port                = 443
+  to_port                  = 443
+  protocol                 = "tcp"
+  source_security_group_id = data.aws_security_group.db_sg.id
+  security_group_id        = aws_security_group.lambda_sg.id
+  description              = "Allow all HTTPS outbound traffic"
 }
 
 #tfsec:ignore:aws-vpc-no-public-egress-sgr
 resource "aws_security_group_rule" "allow_postgres_out" {
-  type              = "egress"
-  from_port         = 5432
-  to_port           = 5432
-  protocol          = "tcp"
-  cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.lambda_sg.id
-  description       = "Allow all Postgres outbound traffic"
+  type                     = "egress"
+  from_port                = 5432
+  to_port                  = 5432
+  protocol                 = "tcp"
+  source_security_group_id = data.aws_security_group.db_sg.id
+  security_group_id        = aws_security_group.lambda_sg.id
+  description              = "Allow all Postgres outbound traffic"
 }
 
 resource "aws_security_group_rule" "database_allow_in_from_lambda" {
