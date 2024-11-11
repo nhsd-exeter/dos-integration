6c295123f2512d8129512f8618307dd4e0717e7e
Thomas Cordall <thomas.cordall@kainos.com>
2024-11-11 14:32:33 +0000

    More logging to sync

--- application/service_sync/service_sync.py
+++ application/service_sync/service_sync.py
@@ -54,10 +54,10 @@ def lambda_handler(event: SQSEvent, context: LambdaContext) -> None:  # noqa: AR
             nhs_entity=nhs_entity,
             service_histories=service_histories,
         )
-        logger.warning("TEST LOG ", nhs_entity.org_sub_type
-        )
-        logger.warning("TEST LOG ", changes_to_dos
-        )
+        logger.info("TEST LOG ", nhs_entity.org_sub_type)
+        logger.info("TEST LOG ", changes_to_dos)
+        logger.warning("TOM TEST LOG", nhs_entity.org_sub_type,
+            cloudwatch_metric_filter_matching_attribute="UpdateRequestSuccess")
         # Update Service History with changes to be made
         service_histories = changes_to_dos.service_histories
         # Update DoS data
