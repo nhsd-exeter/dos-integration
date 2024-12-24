b91710ca863dcf740745e9125c73f3c39b9856c8
Matthew Begley <60427904+mabe13@users.noreply.github.com>
2024-12-24 15:02:37 +0000

    DS-3268 Release pipeline xbuild bug fix (#1037)
    # Task Branch Pull Request
    
    **<https://nhsd-jira.digital.nhs.uk/browse/DS-3268>**
    
    ## Description of Changes
    This PR updates the lambda image builder for pipelines to use an arm
    based CodeBuild job. The 86x version stopped working due to an inability
    to install the docker xbuild plugin. The arm builder we use for task
    environments has not been affect by this issue. As such it makes sense
    to align the non-pipeline builder with the pipeline builder and have
    both CodeBuild jobs use an arm based container to build arm based
    images.
    
    ## Type of change
    
    - Bug fix (non-breaking change which fixes an issue)
    
    ## Development Checklist
    
    - [x] I have performed a self-review of my own code
    - [x] Tests have added that prove my fix is effective or that my feature
    works (Integration tests)
    - [x] I have updated Dependabot to include my changes (if applicable)
    
    ## Code Reviewer Checklist
    
    - [x] I can confirm the changes have been tested or approved by a tester

--- infrastructure/stacks/development-and-deployment-tools/pipeline_stages.tf
+++ infrastructure/stacks/development-and-deployment-tools/pipeline_stages.tf
@@ -65,8 +65,8 @@ resource "aws_codebuild_project" "build_image_stage" {
 
   environment {
     compute_type                = "BUILD_GENERAL1_SMALL"
-    image                       = "aws/codebuild/amazonlinux2-x86_64-standard:5.0"
-    type                        = "LINUX_CONTAINER"
+    image                       = "aws/codebuild/amazonlinux2-aarch64-standard:3.0"
+    type                        = "ARM_CONTAINER"
     image_pull_credentials_type = "CODEBUILD"
     privileged_mode             = true
 
